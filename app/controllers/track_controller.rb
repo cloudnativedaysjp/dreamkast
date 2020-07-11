5fed7d817ea056cbe50338dc49e45d29f27c1e6d
r_takaishi <ryo.takaishi.0@gmail.com>
2020-07-11 14:52:37 +0900

    埋め込むURLも動的に取得

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -7,19 +7,6 @@ class TrackController < ApplicationController
     @event = params[:event]
 
     @new_user = new_user?.to_s
-    case params[:id]
-    when "1" then
-      @movie_url = "https://www.youtube.com/embed/QJSo8BZlbeI"
-    when "2" then
-      @movie_url = "https://www.youtube.com/embed/i28YOIui1bc?autoplay=1"
-    when "3" then
-      @movie_url = "https://www.youtube.com/embed/bqD9EwKaE90?autoplay=1"
-    when "4" then
-      @movie_url = "https://www.youtube.com/embed/hL07TE17rZ8?autoplay=1"
-    when "5" then
-      @movie_url = "https://www.youtube.com/embed/rD9czFIT4Iw?autoplay=1"
-    when "6" then
-      @movie_url = "https://www.youtube.com/embed/sSGVgLmZ8u0?autoplay=1"
-    end
+    @movie_url = @tracks.find_by(number: params[:id].to_i).movie_url
   end
 end

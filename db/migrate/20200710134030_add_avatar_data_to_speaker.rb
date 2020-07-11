72bfd429aa4ca0cc92e5ed190f8bef634d35cc85
r_takaishi <ryo.takaishi.0@gmail.com>
2020-07-11 13:19:53 +0900

    drop table for active storage

--- db/migrate/20200710134030_add_avatar_data_to_speaker.rb
+++ db/migrate/20200710134030_add_avatar_data_to_speaker.rb
@@ -1,5 +1,7 @@
 class AddAvatarDataToSpeaker < ActiveRecord::Migration[6.0]
   def change
     add_column :speakers, :avatar_data, :text
+    drop_table :active_storage_blobs
+    drop_table :active_storage_attachments
   end
 end

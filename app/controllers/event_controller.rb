9c6e844326c3c92a5bfb7da969cb5ad647ce1853
r_takaishi <ryo.takaishi.0@gmail.com>
2020-07-11 23:21:44 +0900

    dynamic redirection

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,7 +1,7 @@
 class EventController < ApplicationController
   def show
     if session[:userinfo].present?
-      redirect_to '/cndt2020/track/1'
+      redirect_to "/#{event_name}/track/1"
     end
   end
 end

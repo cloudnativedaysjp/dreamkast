9f31e7717e2cb985f0cac197ee931ff38cb13a97
r_takaishi <ryo.takaishi.0@gmail.com>
2020-07-13 11:53:48 +0900

    add test: Profiles::TalksController

--- spec/controllers/profiles/talks_controller_spec.rb
+++ spec/controllers/profiles/talks_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Profiles::TalksController, type: :controller do
+  describe "POST #create" do
+    describe "not logged in" do
+      let(:profile)  { create(:profile) }
 
+      it "redirect to /:event" do
+        post :create, params: {event: 'cndt2020'}
+        expect(response).to redirect_to "http://test.host/cndt2020"
+      end
+    end
+
+    describe "logged in and not registered" do
+      before do
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(userinfo: {info: {email: "foo@example.com"}})
+      end
+
+      it "redirect to /:event/registration" do
+        post :create, params: {event: 'cndt2020'}
+        expect(response).to redirect_to registration_path
+      end
+    end
+
+    describe "logged in and registered" do
+      before do
+        create(:alice)
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(userinfo: {info: {email: "foo@example.com"}})
+      end
+
+      it "redirect to /:event/timetables" do
+        post :create, params: {event: 'cndt2020'}
+        expect(response).to redirect_to timetables_path
+      end
+    end
+  end
 end

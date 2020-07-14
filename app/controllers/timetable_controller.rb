c690da242f58ff56116d157db25298b3fb05cad2
r_takaishi <ryo.takaishi.0@gmail.com>
2020-07-14 20:30:06 +0900

    タイムテーブル表示時に実行されるクエリを減らす

--- app/controllers/timetable_controller.rb
+++ app/controllers/timetable_controller.rb
@@ -9,14 +9,26 @@ class TimetableController < ApplicationController
   end
 
   def index
-    @conference = Conference.includes(:talks, {talks: :speakers}).find_by(abbr: event_name)
+    @conference = Conference.includes(:talks).find_by(abbr: event_name)
+    @talks = Talk.eager_load(:talk_category, :talk_difficulty).all
+    @talk_cagetogies = TalkCategory.all
+    @talk_difficulties = TalkDifficulty.all
   end
 
+  # ActiveRecordの機能でもうちょっといい感じにかける気はする…
   def talks_checked?(talk_id)
     @profile.talks.select{|talk| talk.id == talk_id}.present?
   end
 
-  helper_method :talks_checked?
+  def talk_category(talk_id)
+    @talk_cagetogies.find{|category| category.id == @conference.talks.find{|talk| talk.id == talk_id}.talk_category_id}
+  end
+
+  def talk_difficulty(talk_id)
+    @talk_difficulties.find{|difficulty| difficulty.id == @conference.talks.find{|talk| talk.id == talk_id}.talk_difficulty_id}
+  end
+
+  helper_method :talks_checked?, :talk_category, :talk_difficulty
 
   private
 

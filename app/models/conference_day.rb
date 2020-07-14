3bbd89790aee2bf44dcb96ce2c97dcb2305658b0
Kazuto Kusama <jacopen@gmail.com>
2020-07-14 21:41:14 +0900

    Fix talks order

--- app/models/conference_day.rb
+++ app/models/conference_day.rb
@@ -1,3 +1,3 @@
 class ConferenceDay < ApplicationRecord
-  has_many :talks, -> {order('conference_day_id ASC, HOUR(start_time) ASC, MINUTE(start_time) ASC, track ASC')}
+  has_many :talks, -> {order('conference_day_id ASC, HOUR(start_time) ASC, track ASC, MINUTE(start_time) ASC')}
 end

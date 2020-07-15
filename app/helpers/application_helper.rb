9855b6e4a408ad3d000e01a523e144a36f6027c3
Kazuto Kusama <jacopen@gmail.com>
2020-07-16 01:10:21 +0900

    Implement message system

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,13 @@ module ApplicationHelper
     return if logged_in?
     redirect_to root_path, alert: 'ログインしてください'
   end
+
+  def message_box
+    cls = ""
+    unless @message_box
+      @message_box = "CloudNative Days 2020へようこそ"
+      cls = "d-none"
+    end
+    return "<div id=\"message_box\" class=\"#{cls}\"><p>#{@message_box}</p></div>"
+  end
 end

c0c70b5a60b9f966ebeeb4aa0736f89f72f4f8b1
Kazuto Kusama <jacopen@gmail.com>
2020-07-18 23:43:30 +0900

    Re-design timetable submit button

--- app/javascript/packs/message_box.js
+++ app/javascript/packs/message_box.js
@@ -1,6 +1,11 @@
 $(function(){
     $(document).on("click","#message_icon", function(){
-        //var clickEventType = (( window.ontouchstart!==null ) ? 'click':'touchend');
         $("#message_box").toggleClass("d-none");
+    });
+    $(window).scroll(function() {
+        $("#message_box").addClass("d-none");
+    });
+    $(window).touchmove(function() {
+        $("#message_box").addClass("d-none");
     });
 })

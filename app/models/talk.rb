0153596ea365b9bdc45e2290f798b65c864a91e0
Kazuto Kusama <jacopen@gmail.com>
2020-07-19 02:03:38 +0900

    Create relationship between Talk and Track

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -3,13 +3,13 @@ class Talk < ApplicationRecord
   belongs_to :talk_difficulty
   belongs_to :conference
   belongs_to :conference_day
+  belongs_to :track
 
   has_many :talks_speakers
   has_many :registered_talks
   has_many :speakers, through: :talks_speakers
   has_many :profiles, through: :registered_talks
 
-  TRACK_MAP = {"1" => "A", "2" => "B", "3" => "C", "4" => "D", "5" => "E", "6" => "F"}
   SLOT_MAP = ["1200","1400","1500","1600","1700","1800","1900","2000"]
 
   def self.import(file)
@@ -22,15 +22,11 @@ class Talk < ApplicationRecord
   end
 
   def self.updatable_attributes
-    ["id","conference_id","title","abstract","track","talk_category_id","talk_difficulty_id","date","conference_day_id","start_time","end_time","movie_url"]
+    ["id","conference_id","title","abstract","track_id","talk_category_id","talk_difficulty_id","date","conference_day_id","start_time","end_time","movie_url"]
   end
 
   def day
     return self.conference_day_id
-  end
-
-  def track_name
-    return TRACK_MAP[self.track]
   end
 
   def slot_number
@@ -42,7 +38,7 @@ class Talk < ApplicationRecord
   end
 
   def talk_number
-    return day.to_s + track_name + slot_number
+    return day.to_s + self.track.name + slot_number
   end
 
   def row_start
@@ -53,13 +49,13 @@ class Talk < ApplicationRecord
     ((self.end_time - self.start_time).to_i / 60 / 10) + row_start
   end
 
-  def self.find_by_params(day, slot_number_param, track)
+  def self.find_by_params(day, slot_number_param, track_id)
     date = ConferenceDay.find(day.to_i).date
 
     after = Time.zone.parse(SLOT_MAP[slot_number_param.to_i - 1].dup.insert(2, ":")).utc.strftime("%T")
     before = (Time.zone.parse(SLOT_MAP[slot_number_param.to_i].dup.insert(2, ":")) - 60).utc.strftime("%T")
 
-    where(date: date, track: track)
+    where(date: date, track_id: track_id)
       .where("TIME(start_time) BETWEEN '#{after}' AND '#{before}'")
   end
 end

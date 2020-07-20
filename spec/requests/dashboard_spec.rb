1bdd0ddcd56974670adc72f446289934bdc14d9e
r_takaishi <ryo.takaishi.0@gmail.com>
2020-07-20 12:18:37 +0900

    fix text

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Profiles::TalksController, type: :request do
+RSpec.describe DashboardController, type: :request do
   subject(:session) { {userinfo: {info: {email: "foo@example.com", extra: {sub: "aaa"}}, extra: {raw_info: {sub: "aaa", "https://cloudnativedays.jp/roles" => roles}}} } }
   let(:roles) { [] }
 
-  describe "GET /:event/profiles/talks" do
+  describe "GET /:event/dashboard" do
     describe "logged in and registered" do
       before do
         create(:cndt2020)
@@ -16,26 +16,26 @@ RSpec.describe Profiles::TalksController, type: :request do
         let(:roles) { ["CNDT2020-Admin"] }
 
         it "return a success response" do
-          get '/cndt2020/profiles/talks'
+          get '/cndt2020/dashboard'
           expect(response).to be_successful
           expect(response).to have_http_status '200'
         end
 
         it "link to admin is displayed" do
-          get '/cndt2020/profiles/talks'
+          get '/cndt2020/dashboard'
           expect(response.body).to include('<a class="dropdown-item" href="/admin">管理画面</a>')
         end
       end
 
       context "user is not admin" do
         it "return a success response" do
-          get '/cndt2020/profiles/talks'
+          get '/cndt2020/dashboard'
           expect(response).to be_successful
           expect(response).to have_http_status '200'
         end
 
         it "link to admin is not displayed" do
-          get '/cndt2020/profiles/talks'
+          get '/cndt2020/dashboard'
           expect(response.body).to_not include('<a class="dropdown-item" href="/admin">管理画面</a>')
         end
       end

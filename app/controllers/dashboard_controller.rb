c8d1e7328edffdceff733d5c3aabb6901864d281
r_takaishi <ryo.takaishi.0@gmail.com>
2020-07-20 12:09:46 +0900

    redirect to dashboard before login

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,17 @@
 class DashboardController < ApplicationController
   include Secured
-  include Logging
-  
+  before_action :set_profile
+
   def show
-    @user = session[:userinfo]
+    @conference = Conference.includes(:talks).find_by(abbr: event_name)
+    @talks = Talk.eager_load(:talk_category, :talk_difficulty).all
+    @talk_cagetogies = TalkCategory.all
+    @talk_difficulties = TalkDifficulty.all
+  end
+
+  private
+
+  def set_profile
+    @profile = Profile.find_by(email: @current_user[:info][:email])
   end
 end

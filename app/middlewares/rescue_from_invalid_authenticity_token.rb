b4a97320a0046fe2f9a2174d5e9585860c6304ed
Kazuto Kusama <jacopen@gmail.com>
2020-07-24 11:08:57 +0900

    Remove comment

--- app/middlewares/rescue_from_invalid_authenticity_token.rb
+++ app/middlewares/rescue_from_invalid_authenticity_token.rb
@@ -1,14 +1,12 @@
-#module Middleware
-  class RescueFromInvalidAuthenticityToken
-    def initialize(app)
-      @app = app 
-    end 
-    
-    def call(env)
-      # yield
-      @app.call(env)
-    rescue ActionController::InvalidAuthenticityToken
-      [302, {'Location' => "/", 'Content-Type' => 'text/html'}, ['Invalid Authenticity Token']]
-    end
+class RescueFromInvalidAuthenticityToken
+  def initialize(app)
+    @app = app 
+  end 
+  
+  def call(env)
+    # yield
+    @app.call(env)
+  rescue ActionController::InvalidAuthenticityToken
+    [302, {'Location' => "/", 'Content-Type' => 'text/html'}, ['Invalid Authenticity Token']]
   end
-#end
+end

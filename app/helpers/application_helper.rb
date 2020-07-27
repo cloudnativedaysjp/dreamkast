049fbcf28fd3642c7c1dbf207f116ac8ef3e8bcf
inductor <kohei.ota@hpe.com>
2020-07-27 18:27:58 +0900

    implement ogp and twitter card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,4 +30,22 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  def full_title(page_title = '')
+    base_title = 'CloudNative Days Tokyo 2020'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
+  def full_url(path)
+    domain = if Rails.env.development?
+               'http://0.0.0.0:3000'
+             else
+               'https://event.cloudnativedays.jp/cndt2020'
+             end
+    "#{domain}#{path}"
+  end
 end

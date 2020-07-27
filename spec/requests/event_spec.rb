a0bc8a0da7eade687220c6e277cca700ecda528e
Kazuto Kusama <jacopen@gmail.com>
2020-07-27 22:43:12 +0900

    Fix-redirect-path

--- spec/requests/event_spec.rb
+++ spec/requests/event_spec.rb
@@ -34,7 +34,7 @@ describe EventController, type: :request do
         allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(userinfo: {info: {email: "foo@example.com"}})
       end
 
-      it "redirect to /:event/profiles/talks" do
+      it "redirect to /:event/dashboard" do
         get '/cndt2020'
         expect(response).to_not be_successful
         expect(response).to have_http_status '302'

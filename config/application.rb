92d2448afef4fb6349670c006acdb25de6180f2e
r_takaishi <ryo.takaishi.0@gmail.com>
2020-07-30 17:06:24 +0900

    don't use

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,6 @@ module Cndtattend
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     config.active_model.i18n_customize_full_message = true
     config.autoload_paths << "#{Rails.root}/app/middlewares"
-    config.autoload_paths << "#{Rails.root}/app/models/**/"
   end
 end
 

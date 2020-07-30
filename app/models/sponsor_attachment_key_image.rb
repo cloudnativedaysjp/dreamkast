8074cd49deb5f955cd2d158e54b246d2d17fd4b3
r_takaishi <ryo.takaishi.0@gmail.com>
2020-07-30 16:44:40 +0900

    add sponsor attachment: type key_image

---
+++ app/models/sponsor_attachment_key_image.rb
@@ -0,0 +1,13 @@
+class SponsorAttachmentKeyImage < SponsorAttachment
+  include SponsorAttachmentFileUploader::Attachment(:file)
+
+  MAX_POSTS_COUNT = 2
+
+  validate :posts_count_must_be_within_limit
+
+  private
+
+  def posts_count_must_be_within_limit
+    errors.add(:base, "posts count limit: #{MAX_POSTS_COUNT}") if sponsor.sponsor_attachment_key_images.count >= MAX_POSTS_COUNT
+  end
+end

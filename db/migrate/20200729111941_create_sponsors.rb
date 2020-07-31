4b62a8e3a084e35b25d19883d34cd99c49801f45
r_takaishi <ryo.takaishi.0@gmail.com>
2020-07-31 10:33:29 +0900

    sponsor has url

--- db/migrate/20200729111941_create_sponsors.rb
+++ db/migrate/20200729111941_create_sponsors.rb
@@ -3,6 +3,7 @@ class CreateSponsors< ActiveRecord::Migration[6.0]
     create_table :sponsors do |t|
       t.string :name
       t.text :description
+      t.string :url
       t.belongs_to :conference, null: false, foreign_key: true
 
       t.timestamps

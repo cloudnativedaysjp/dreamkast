171a823dbcb50279bbb2815c1e513aeaef4a534d
r_takaishi <ryo.takaishi.0@gmail.com>
2020-08-01 13:23:18 +0900

    movie_urlは一旦除外

--- db/fixtures/seeds.rb
+++ db/fixtures/seeds.rb
@@ -73,12 +73,12 @@ FormItem.seed(
 )
 
 Track.seed(
-  { id: 1, number: 1, name: "A", conference_id: 1, movie_url: ""},
-  { id: 2, number: 2, name: "B", conference_id: 1, movie_url: ""},
-  { id: 3, number: 3, name: "C", conference_id: 1, movie_url: ""},
-  { id: 4, number: 4, name: "D", conference_id: 1, movie_url: ""},
-  { id: 5, number: 5, name: "E", conference_id: 1, movie_url: ""},
-  { id: 6, number: 6, name: "F", conference_id: 1, movie_url: ""}
+  { id: 1, number: 1, name: "A", conference_id: 1},
+  { id: 2, number: 2, name: "B", conference_id: 1},
+  { id: 3, number: 3, name: "C", conference_id: 1},
+  { id: 4, number: 4, name: "D", conference_id: 1},
+  { id: 5, number: 5, name: "E", conference_id: 1},
+  { id: 6, number: 6, name: "F", conference_id: 1}
 )
 
 TalkCategory.seed(

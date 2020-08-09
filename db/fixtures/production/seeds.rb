43d35a5bd5f2f25c28295727bb1be45e46060b70
Kazuto Kusama <jacopen@gmail.com>
2020-08-09 20:12:15 +0900

    Refresh TalksSpekaer while db:seeds

--- db/fixtures/production/seeds.rb
+++ db/fixtures/production/seeds.rb
@@ -5,6 +5,7 @@ if ENV['REVIEW_APP'] == 'true'
   csv = CSV.read(File.join(Rails.root, 'db/speakers.csv'), headers: true)
   Speaker.seed(csv.map(&:to_hash))
 
+  TalksSpeaker.delete_all
   csv = CSV.read(File.join(Rails.root, 'db/talks_speakers.csv'), headers: true)
   TalksSpeaker.seed(csv.map(&:to_hash))
   

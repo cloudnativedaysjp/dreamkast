cf0f1b738630beb69a3509a25105f61305188795
r_takaishi <ryo.takaishi.0@gmail.com>
2020-08-11 17:02:20 +0900

    refactor: extract to function

--- app/javascript/packs/sponsor_form.js
+++ app/javascript/packs/sponsor_form.js
@@ -1,40 +1,36 @@
 $(document).on('click', '.add_pdf_fields', function(event) {
-    var regexp, time;
-    time = new Date().getTime();
-    regexp = new RegExp($(this).data('id'), 'g');
-    $('.pdf-fields').append($(this).data('fields').replace(regexp, time));
-    if ($('div:visible.pdf-field').length >= 3) {
-        $('.add-pdf').hide()
-    }
+    add_field('pdf', 3);
     return event.preventDefault();
 })
 
 $(document).on('click', '.remove_pdf_field', function(event) {
-    $(this).prev('input[type=hidden]').val('1');
-    $(this).closest('.pdf-field').hide();
-    if ($('div:visible.pdf-field').length < 3) {
-        $('.add-pdf').show()
-    }
+    remove_field('pdf', 3);
     return event.preventDefault();
 });
 
 $(document).on('click', '.add_key_image_fields', function(event) {
-    var regexp, time;
-    time = new Date().getTime();
-    regexp = new RegExp($(this).data('id'), 'g');
-    $('.key-image-fields').append($(this).data('fields').replace(regexp, time));
-    if ($('div:visible.key-image-field').length >= 2) {
-        $('.add-key-image').hide()
-    }
+    add_field('key-image', 2);
     return event.preventDefault();
 })
 
 $(document).on('click', '.remove_key_image_field', function(event) {
-    console.log('remove_key_image_field')
-    $(this).prev('input[type=hidden]').val('1');
-    $(this).closest('.key-image-field').hide();
-    if ($('div:visible.key-image-field').length < 2) {
-        $('.add-key-image').show()
-    }
+    remove_field('key-image', 2);
     return event.preventDefault();
 });
+
+function add_field(name, max) {
+    let time = new Date().getTime();
+    let regexp = new RegExp($(this).data('id'), 'g');
+    $('.'+name+'fields').append($(this).data('fields').replace(regexp, time));
+    if ($('div:visible.'+name+'-field').length >= max) {
+        $('.add-'+name+'pdf').hide()
+    }
+}
+
+function remove_field(name, max) {
+    $(this).prev('input[type=hidden]').val('1');
+    $(this).closest('.'+name+'-field').hide();
+    if ($('div:visible.'+name+'-field').length < max) {
+        $('.add-'+name).show()
+    }
+}

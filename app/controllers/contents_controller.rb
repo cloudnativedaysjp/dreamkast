9cbc5318cf108beef3794479c61fc29f935e717a
MasayaAoyama <amsy810@gmail.com>
2020-08-17 16:31:51 +0900

    add contents controller for auth

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,4 +1,19 @@
 class ContentsController < ApplicationController
+  include Secured
+  before_action :set_profile
+
+  def logged_in_using_omniauth?
+    if session[:userinfo].present?
+      @current_user = session[:userinfo]
+    end
+  end
+
   def index
+  end
+
+  private
+
+  def set_profile
+    @profile = Profile.find_by(email: @current_user[:info][:email])
   end
 end

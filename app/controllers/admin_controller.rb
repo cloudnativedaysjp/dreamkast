a22415060844da28c0d977b11d7fcb224158d003
r_takaishi <ryo.takaishi.0@gmail.com>
2020-08-20 16:27:32 +0900

    refactor: talksを切り出し

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -26,55 +26,7 @@ class AdminController < ApplicationController
         @profiles = Profile.all
     end
 
-    def talks
-        @talks = Talk.all.order('conference_day_id ASC, start_time ASC, track_id ASC')
-    end
-
-    def update_talks
-        params[:video].each do |talk_id, value|
-            talk = Talk.find(talk_id)
-            if talk.video
-                video = Talk.find(talk_id).video
-            else
-                video = Video.new
-                video.talk_id = talk.id
-            end
-            video.video_id = value[:video_id]
-            video.slido_id = value[:slido_id]
-            if value[:on_air]
-                video.on_air = true 
-            else
-                video.on_air = false 
-            end
-            video.save
-        end
-        ActionCable.server.broadcast(
-            "track_channel", Video.on_air
-        )
-
-        redirect_to '/admin/talks', notice: "配信設定を更新しました"
-    end
-
     def statistics
-    end
-
-    def bulk_insert_talks
-        unless params[:file]
-            redirect_to '/admin/talks', notice: "アップロードするファイルを選択してください"
-        else
-            message = Talk.import(params[:file])
-            notice = message.join(" / ")
-            redirect_to '/admin/talks', notice: notice
-        end
-    end
-
-    def bulk_insert_talks_speaker
-        unless params[:file]
-            redirect_to '/admin/talks', notice: "アップロードするファイルを選択してください"
-        else
-            TalksSpeaker.import(params[:file])
-            redirect_to '/admin/talks', notice: 'CSVの読み込みが完了しました'
-        end
     end
 
     def export_statistics

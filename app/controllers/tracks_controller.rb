b13b8d6b248f2fdfabee1e2fcdc8a913e060a633
r_takaishi <ryo.takaishi.0@gmail.com>
2020-08-28 11:33:48 +0900

    show booths in tracks page.

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,8 +1,10 @@
 class TracksController < ApplicationController
 
   def index
+    @conference = Conference.includes(:talks).find_by(abbr: event_name)
     @current = Video.on_air
     @tracks = Track.all
+    @booths = Booth.where(conference_id: @conference.id)
   end
 
   def blank

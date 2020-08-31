92a75bc812a5f8677f7e41c299048d2b38baf253
Kazuto Kusama <jacopen@gmail.com>
2020-09-01 02:11:21 +0900

    Tuuuuuuune

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,6 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-workers = ENV.fetch("WEB_CONCURRENCY") { 2 }
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -27,6 +26,9 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+workers = ENV.fetch("WEB_CONCURRENCY") { 2 }
+preload_app!
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code

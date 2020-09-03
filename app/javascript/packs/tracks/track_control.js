1c7c1ad5f7615282977b59b8e1a9a4ea02bf75a6
Kazuto Kusama <jacopen@gmail.com>
2020-09-03 22:20:46 +0900

    Set default movie

--- app/javascript/packs/tracks/track_control.js
+++ app/javascript/packs/tracks/track_control.js
@@ -1,14 +1,14 @@
 window.update_track = function(track){
     clearInterval(window.timer);
     if(track === undefined || track === null){
-        document.getElementById("video").contentWindow.location.replace("/cndt2020/tracks/blank");
+        document.getElementById("video").contentWindow.location.replace("https://player.vimeo.com/video/450845161?autoplay=1&loop=0&autopause=0");
         document.getElementById("slido").contentWindow.location.replace("/cndt2020/tracks/blank");
         document.getElementById("twitter").href = "http://twitter.com/share?url=https://event.cloudnativedays.jp/cndt2020/&related=@cloudnativedays&hashtags=CNDT2020";
         document.getElementById("twitter").innerHTML = "Twitterでつぶやく<br/>#CNDT2020";
-        document.getElementById("title").innerHTML = "放送なし";
+        document.getElementById("title").innerHTML = "放送開始までしばらくお待ちください";
         document.getElementById("abstract").innerHTML = "";
         document.getElementById("speakers").innerHTML = "";
-        document.getElementById("time").innerHTML = "";
+        document.getElementById("time").innerHTML = "現在このトラックに放送中のセッションはありません。<br/>次のセッションをお待ちいただくか、他のトラックをご覧ください。";
         window.selected_talk_id = "0";
     }else{
         document.getElementById("video").contentWindow.location.replace("https://player.vimeo.com/video/" + track.video_id + "?autoplay=1&loop=0&autopause=0");

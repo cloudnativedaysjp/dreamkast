8b61c3cbb031f456bf1d776a8dcbd8c043c4f9e4
Kazuto Kusama <jacopen@gmail.com>
2020-09-08 02:00:22 +0900

    Fix ci

--- spec/requests/contents_spec.rb
+++ spec/requests/contents_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe ContentsController, type: :request do
-  describe "GET #index" do
+  describe "GET #kontest" do
 
     describe "logged in and registered" do
       subject(:session) { {userinfo: {info: {email: "foo@example.com", extra: {sub: "aaa"}}, extra: {raw_info: {sub: "aaa", "https://cloudnativedays.jp/roles" => roles}}} } }
@@ -13,8 +13,8 @@ describe ContentsController, type: :request do
         allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(session)
       end
 
-      it "returns a success response with contents (sub event) list" do
-        get '/cndt2020/contents'
+      it "returns a success response with kontest (sub event) list" do
+        get '/cndt2020/kontest'
         expect(response).to be_successful
         expect(response).to have_http_status '200'
         expect(response.body).to include 'https://'
@@ -28,7 +28,7 @@ describe ContentsController, type: :request do
       end
 
       it "redirect to /cndt2020/registration" do
-        get '/cndt2020/contents'
+        get '/cndt2020/kontest'
         expect(response).to_not be_successful
         expect(response).to have_http_status '302'
         expect(response).to redirect_to '/cndt2020/registration'
@@ -40,11 +40,60 @@ describe ContentsController, type: :request do
         create(:cndt2020)
       end
 
-      it "returns a success response with contents (sub event) list" do
-        get '/cndt2020/contents'
+      it "returns a success response with kontest (sub event) list" do
+        get '/cndt2020/kontest'
         expect(response).to be_successful
         expect(response).to have_http_status '200'
-        expect(response.body).to include '併設イベント'
+        expect(response.body).to include 'kontest'
+      end
+    end
+
+  end
+
+  describe "GET #discussion" do
+
+    describe "logged in and registered" do
+      subject(:session) { {userinfo: {info: {email: "foo@example.com", extra: {sub: "aaa"}}, extra: {raw_info: {sub: "aaa", "https://cloudnativedays.jp/roles" => roles}}} } }
+      let(:roles) { [] }
+
+      before do
+        create(:cndt2020)
+        create(:alice)
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(session)
+      end
+
+      it "returns a success response with discussion" do
+        get '/cndt2020/discussion'
+        expect(response).to be_successful
+        expect(response).to have_http_status '200'
+        expect(response.body).to include '書き込む'
+      end
+    end
+
+    describe 'logged in and not registerd' do
+      before do
+        create(:cndt2020)
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(userinfo: {info: {email: "foo@example.com"}})
+      end
+
+      it "redirect to /cndt2020/registration" do
+        get '/cndt2020/discussion'
+        expect(response).to_not be_successful
+        expect(response).to have_http_status '302'
+        expect(response).to redirect_to '/cndt2020/registration'
+      end
+    end
+
+    describe "not logged in" do
+      before do
+        create(:cndt2020)
+      end
+
+      it "returns a success response with discussion" do
+        get '/cndt2020/discussion'
+        expect(response).to be_successful
+        expect(response).to have_http_status '200'
+        expect(response.body).to include '使い方'
       end
     end
 

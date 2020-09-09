ec28073e889f9c03463bece0224614e1c6a926c9
r_takaishi <ryo.takaishi.0@gmail.com>
2020-09-09 16:28:48 +0900

    カンファレンス終了後の画面を変更

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,16 +1,6 @@
 class LinksController < ApplicationController
-  include Secured
-  before_action :set_profile
-
   def index
     @conference = Conference.includes(talks: [:track, :speakers, :conference_day]).find_by(abbr: event_name)
     @links = Link.where(conference_id: @conference.id)
-  end
-
-  private
-  def set_profile
-    if @current_user
-      @profile = Profile.find_by(email: @current_user[:info][:email])
-    end
   end
 end

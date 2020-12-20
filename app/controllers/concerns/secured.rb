6907c4aad333160b06a92ab43af350320db5a47e
r_takaishi <ryo.takaishi.0@gmail.com>
2020-12-20 11:35:38 +0900

    不要な変更なので戻す

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -5,31 +5,13 @@ module Secured
       before_action :logged_in_using_omniauth?, :new_user?
       helper_method :admin?, :speaker?
     end
-  
+
     def logged_in_using_omniauth?
-      p action_name
-      if logged_in?
+      if session[:userinfo].present?
         @current_user = session[:userinfo]
-      end
-
-      if controller_name == 'speakers'
-        if !logged_in? && ["speakers"].include?(controller_name)
-          puts "redirect_to /auth/auth0"
-          redirect_to "/auth/auth0"
-        end
-
-        if new_user? && !speakers_new? && !speakers_create?
-          puts "redirect_to /#{params[:event]}/speakers/registration"
-          redirect_to "/#{params[:event]}/speakers/registration"
-        end
       else
-
-        unless logged_in?
-          puts "redirect_to /#{params[:event]}"
-          redirect_to "/#{params[:event]}"
-        end
+        redirect_to "/#{params[:event]}"
       end
-
     end
 
     def new_user?
@@ -40,24 +22,12 @@ module Secured
       end
     end
 
-    def speakers_new?
-      controller_name == 'speakers' && action_name == 'new'
-    end
-
-    def speakers_create?
-      controller_name == 'speakers' && action_name == 'create'
-    end
-
     def admin?
       @current_user[:extra][:raw_info]["https://cloudnativedays.jp/roles"].include?("CNDT2020-Admin")
     end
 
     def speaker?
       @current_user[:extra][:raw_info]["https://cloudnativedays.jp/roles"].include?("CNDT2020-Speaker")
-    end
-
-    def logged_in?
-      session[:userinfo].present?
     end
 
     def is_admin?
--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -19,44 +19,25 @@ class SpeakersController < ApplicationController
   # GET /speakers/1
   # GET /speakers/1.json
   def show
-    @speaker = Speaker.find_by(id: params[:id])
-    render_404 unless @speaker
-    authorize @speaker
-
-    @conference = Conference.find_by(abbr: params[:event])
   end
 
   # GET /speakers/new
   def new
-    @speaker_form = SpeakerForm.new
-    @conference = Conference.find_by(abbr: params[:event])
-    @speaker_form.load
+    @speaker = Speaker.new
   end
 
   # GET /speakers/1/edit
   def edit
-    @speaker = Speaker.find_by(id: params[:id])
-    authorize @speaker
-
-    @speaker_form = SpeakerForm.new(speaker: @speaker)
-    @speaker_form.load
-
-    @conference = Conference.find_by(abbr: params[:event])
   end
 
   # POST /speakers
   # POST /speakers.json
   def create
-    @conference = Conference.find_by(abbr: params[:event])
-
-    @speaker_form = SpeakerForm.new(speaker_params, speaker: Speaker.new())
-    @speaker_form.sub = @current_user[:extra][:raw_info][:sub]
-    @speaker_form.email = @current_user[:info][:email]
-
+    @speaker = Speaker.new(speaker_params)
 
     respond_to do |format|
-      if @speaker_form.save
-        format.html { redirect_to "/#{@conference.abbr}", notice: 'Speaker was successfully created.' }
+      if @speaker.save
+        format.html { redirect_to @speaker, notice: 'Speaker was successfully created.' }
         format.json { render :show, status: :created, location: @speaker }
       else
         format.html { render :new }
@@ -68,15 +49,9 @@ class SpeakersController < ApplicationController
   # PATCH/PUT /speakers/1
   # PATCH/PUT /speakers/1.json
   def update
-    @speaker = Speaker.find(params[:id])
-    authorize @speaker
-
-    @speaker_form = SpeakerForm.new(speaker_params, speaker: @speaker)
-    @speaker_form.load
-
     respond_to do |format|
-      if @speaker_form.update(speaker_params)
-        format.html { redirect_to speaker_path(id: @speaker.id), notice: 'Speaker was successfully updated.' }
+      if @speaker.update(speaker_params)
+        format.html { redirect_to @speaker, notice: 'Speaker was successfully updated.' }
         format.json { render :show, status: :ok, location: @speaker }
       else
         format.html { render :edit }
@@ -107,35 +82,8 @@ class SpeakersController < ApplicationController
     end
   end
 
-  helper_method :speaker_url
-
-  def speaker_url
-    case action_name
-    when 'new'
-      "/#{params[:event]}/speakers"
-    when 'edit'
-      "/#{params[:event]}/speakers/#{params[:id]}"
-    end
-  end
-
-  def pundit_user
-    if @current_user
-      Speaker.find_by(email: @current_user[:info][:email])
-    end
-  end
-
   # Only allow a list of trusted parameters through.
   def speaker_params
-    params.require(:speaker).permit(:name,
-                                    :sub,
-                                    :email,
-                                    :profile,
-                                    :company,
-                                    :job_title,
-                                    :twitter_id,
-                                    :github_id,
-                                    :avatar,
-                                    :conference_id,
-                                    talks_attributes: [:id, :title, :abstract, :conference_id, :_destroy])
+    params.require(:speaker).permit(:name, :profile, :company, :job_title, :twitter_id, :github_id, :avatar)
   end
 end

0b595d681525eeda5078a82bd39edc29c5ec5241
r_takaishi <ryo.takaishi.0@gmail.com>
2020-12-27 17:06:41 +0900

    フォームを隠す際にrequireなどを消す

--- app/javascript/packs/speaker_form.js
+++ app/javascript/packs/speaker_form.js
@@ -14,6 +14,9 @@ $(document).on('click', '.remove_talk_field', function(event) {
     console.log("remove talk field!!")
     $(this).parent().find('.destroy_flag_field').val(1)
     $(this).closest('.talk-field').hide();
+    $(this).parent().find('input').removeAttr('required max min maxlength pattern');
+    $(this).parent().find('textarea').removeAttr('required max min maxlength pattern');
+    $(this).parent().find('select').removeAttr('required max min maxlength pattern');
     if ($('div:visible.talk-field').length < 3) {
         $('.add-talk').show()
     }

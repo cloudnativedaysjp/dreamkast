63f9baf6c741f78686869b162c2b5ecf23518374
r_takaishi <ryo.takaishi.0@gmail.com>
2021-01-13 19:30:53 +0900

    It's allowed that abstract is empty

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -14,7 +14,10 @@ class Talk < ApplicationRecord
 
   validates :conference_id, presence: true
   validates :title, presence: true
-  validates :abstract, presence: true
+
+  # エントリー時、セッション概要は空白でもいいのでバリデーションしなくていい
+  # validates :abstract, presence: true
+
   # エントリーフォームから入力する時、以下のフィールドには入力しないのでバリデーションを削っている
   # validates :track_id, presence: true
   # validates :talk_category_id, presence: true

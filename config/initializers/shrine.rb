b21a1bef3910618061a3550aeadee744eaf2b49c
Kazuto Kusama <jacopen@gmail.com>
2021-01-23 17:06:01 +0900

    Support Heroku & other container environment

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -8,12 +8,24 @@ if Rails.env.development? || Rails.env.test?
     store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),
   }
 else
-  s3_options = {
-    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_ACCESS_KEY_ID'],
-    region: ENV['S3_REGION'],
-    bucket: ENV['S3_BUCKET']
-  }
+  s3_options = {}
+  if ENV['CLOUDCUBE_ACCESS_KEY_ID']
+    s3_options = {
+      access_key_id: ENV['CLOUDCUBE_ACCESS_KEY_ID'],
+      secret_access_key: ENV['CLOUDCUBE_SECRET_ACCESS_KEY'],
+      region: 'us-east-1',
+      bucket: URI.parse(ENV['CLOUDCUBE_URL']).host.split('.')[0],
+      prefix: URI.parse(ENV['CLOUDCUBE_URL']).path.slice(1...)
+    }
+  else
+    s3_options = {
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_ACCESS_KEY_ID'],
+      region: ENV['S3_REGION'],
+      bucket: ENV['S3_BUCKET']
+    }
+  end
+  
 
   Shrine.storages = {
     cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),

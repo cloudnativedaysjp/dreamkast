200f2e4a66075b9a29b6f4ed84d71f81b1055fb5
r_takaishi <ryo.takaishi.0@gmail.com>
2021-01-28 23:49:05 +0900

    add talk show api

--- app/controllers/api/v1/talks_controller.rb
+++ app/controllers/api/v1/talks_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::TalksController < ApplicationController
     @talks = Talk.where(conference_id: conference.id)
     render 'api/v1/talks/index.json.jbuilder'
   end
+
+  def show
+    @talk = Talk.find(params[:id])
+    render 'api/v1/talks/show.json.jbuilder'
+  end
 end

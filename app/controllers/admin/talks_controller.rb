135b476420ad23b289286ef767bec8b6e3d57fd8
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-01 18:46:59 +0900

    CSVでダウンロードするためのリンクを追加

--- app/controllers/admin/talks_controller.rb
+++ app/controllers/admin/talks_controller.rb
@@ -7,10 +7,34 @@ class Admin::TalksController < ApplicationController
 
   def index
     @talks = @conference.talks.order('conference_day_id ASC, start_time ASC, track_id ASC')
+    respond_to do |format|
+      format.html
+      format.csv do
+        head :no_content
+
+        filename = "#{@conference.abbr}_talks"
+        columns = %w[title abstract speaker talk_time]
+
+        csv = CSV.generate do |csv|
+          #カラム名を1行目として入れる
+          csv << columns
+
+          @talks.each do |talk|
+            csv << [talk.title, talk.abstract, talk.speakers.map(&:name).join(", "), talk.time]
+          end
+        end
+
+        File.open("./#{filename}.csv", "w", encoding: "SJIS") do |file|
+          file.write(csv)
+        end
+        stat = File::stat("./#{filename}.csv")
+        send_file("./#{filename}.csv", filename: "#{filename}.csv", length: stat.size)
+      end
+    end
   end
 
   def update_talks
-    TalksHelper.update_talks(params[:video])
+  TalksHelper.update_talks(params[:video])
 
     redirect_to admin_talks_url, notice: "配信設定を更新しました"
   end

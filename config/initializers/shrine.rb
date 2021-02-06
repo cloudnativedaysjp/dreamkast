33b1f201caeb47a146eb7602393090e4d8c25df9
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-06 19:56:24 +0900

    環境変数名が間違っている

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -22,7 +22,7 @@ else
   else
     s3_options = {
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: ENV['S3_REGION'],
       bucket: ENV['S3_BUCKET']
     }

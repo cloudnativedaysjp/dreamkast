532baed0548b8c89bdf8b39761ed12cb5cfdb823
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-07 17:13:08 +0900

    created_fromは必須ではない

--- app/controllers/api/v1/chat_messages_controller.rb
+++ app/controllers/api/v1/chat_messages_controller.rb
@@ -2,11 +2,13 @@ class Api::V1::ChatMessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    conference_id = params[:eventId]
+    conference = Conference.find(params[:eventId])
     talk_id = params[:talkId]
     created_from = params[:createdFrom]
-    conference = Conference.find(conference_id)
-    @chat_messages = ChatMessage.where(conference_id: conference.id, talk_id: talk_id, created_at: DateTime.parse(created_from)...)
+    @chat_messages = ChatMessage.where(conference_id: conference.id, talk_id: talk_id)
+    if created_from
+      @chat_messages = @chat_messages.where(created_at: DateTime.parse(created_from)...)
+    end
     render 'api/v1/chat_messages/index.json.jbuilder'
   end
 

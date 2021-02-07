960fb2e50f396684f875d4b1609758ead41ba217
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-07 16:28:26 +0900

    update fadctory

--- spec/factories/conferences.rb
+++ spec/factories/conferences.rb
@@ -10,6 +10,7 @@ FactoryBot.define do
     status { 0 }
     speaker_entry { 1 }
     attendee_entry { 1 }
+    show_timetable { 1 }
     about { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -28,6 +29,7 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
     status { 1 }
     speaker_entry { 1 }
     attendee_entry { 1 }
+    show_timetable { 1 }
     about { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -45,6 +47,7 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
     status { 2 }
     speaker_entry { 1 }
     attendee_entry { 1 }
+    show_timetable { 1 }
     about { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -62,6 +65,7 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
     status { 2 }
     speaker_entry { 1 }
     attendee_entry { 1 }
+    show_timetable { 1 }
     about { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.

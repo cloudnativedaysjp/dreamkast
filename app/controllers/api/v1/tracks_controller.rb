5af1afa82c9f25481342805cce137fb5065086d8
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-11 22:45:37 +0900

    eventAbbrに統一

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TracksController < ApplicationController
   def index
-    conference = Conference.find_by(abbr: params[:eventId])
+    conference = Conference.find_by(abbr: params[:eventAbbr])
     @tracks = Track.where(conference_id: conference.id)
     render 'api/v1/tracks/index.json.jbuilder'
   end
--- app/views/api/v1/chat_messages/index.json.jbuilder
+++ app/views/api/v1/chat_messages/index.json.jbuilder
@@ -1,7 +1,7 @@
 json.array! @chat_messages do |msg|
   json.id msg.id
   json.body msg.body
-  json.eventId msg.conference_id
+  json.eventAbbr @conference.abbr
   json.talkId msg.talk_id
   json.boothId msg.booth_id
   json.createdAt msg.created_at.utc

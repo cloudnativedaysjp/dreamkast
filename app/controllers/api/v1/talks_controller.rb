b9eea18dea7c67d5d88f884495e13c867f90247f
Kazuto Kusama <jacopen@gmail.com>
2021-02-13 22:55:10 +0900

    Support trackId param

--- app/controllers/api/v1/talks_controller.rb
+++ app/controllers/api/v1/talks_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::TalksController < ApplicationController
   def index
     conference = Conference.find_by(abbr: params[:eventAbbr])
-    @talks = Talk.where(conference_id: conference.id)
+    query = {conference_id: conference.id}
+    query[:track_id] = params[:trackId] if params[:trackId]
+    @talks = Talk.where(query)
     render 'api/v1/talks/index.json.jbuilder'
   end
 
--- spec/requests/api/v1/talk_index_spec.rb
+++ spec/requests/api/v1/talk_index_spec.rb
@@ -8,12 +8,27 @@ describe TalksController, type: :request do
       create(:day2)
       create(:talk1)
       create(:talk2)
+      create(:talk3)
     end
 
     it 'confirm json schema' do
       get '/api/v1/talks?eventAbbr=cndt2020'
       expect(response).to have_http_status :ok
       assert_response_schema_confirm
+      expect(response.body).to include "talk1"
+      expect(response.body).to include "talk3"
+    end
+
+    it 'have all talks when it doesnt specify trackId' do
+      get '/api/v1/talks?eventAbbr=cndt2020'
+      expect(response.body).to include "talk1"
+      expect(response.body).to include "talk3"
+    end
+
+    it 'only have talks belongs to track1 when it specify trackId=1' do
+      get '/api/v1/talks?eventAbbr=cndt2020&trackId=1'
+      expect(response.body).to include "talk1"
+      expect(response.body).not_to include "talk3"
     end
 
     it 'successed request' do

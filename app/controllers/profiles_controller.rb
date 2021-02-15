a23ddb5a9c12c8c73118e89ce91b0bc7c84bcea4
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-15 18:56:01 +0900

    参加者としてエントリーする際、ログインしていないとフォームに飛ばずイベントページにリダイレクトされる問題を修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
 
   before_action :set_conference
   before_action :set_current_profile, only: [:edit, :update, :destroy]
+  skip_before_action :logged_in_using_omniauth?, only: [:new]
   before_action :is_admin?, :find_profile, only: [:destroy_id, :set_role]
 
   def new

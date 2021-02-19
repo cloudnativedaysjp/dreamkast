14d7cda3b21104c6978b298580d515b53235b3d0
Kazuto Kusama <jacopen@gmail.com>
2021-02-19 21:57:06 +0900

    Revert "UIとあわせてチャット機能を実装するためにあれこれ修正"

--- app/controllers/api/v1/chat_messages_controller.rb
+++ app/controllers/api/v1/chat_messages_controller.rb
@@ -2,26 +2,21 @@ class Api::V1::ChatMessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @conference = Conference.find_by(abbr: params[:eventAbbr])
-    query = {conference_id: @conference.id, room_type: params[:roomType], room_id: params[:roomId]}
-    query[:created_at] = params[:createdFrom] if params[:createdFrom]
-    query[:reply_to] = params[:replyTo] if params[:replyTo]
-    @chat_messages = ChatMessage.where(query)
+    @conference = Conference.find_by(params[:eventAbbr])
+    talk_id = params[:talkId]
+    created_from = params[:createdFrom]
+    @chat_messages = ChatMessage.where(conference_id: @conference.id, talk_id: talk_id)
+    if created_from
+      @chat_messages = @chat_messages.where(created_at: DateTime.parse(created_from)...)
+    end
     render 'api/v1/chat_messages/index.json.jbuilder'
   end
 
   def create
     @params ||= JSON.parse(request.body.read, {:symbolize_names => true})
-    conference = Conference.find_by(abbr: params[:eventAbbr])
-    room_id = @params[:roomId]
-    room_type = @params[:roomType]
+    conference = Conference.find_by(params[:eventAbbr])
+    talk_id = @params[:talkId]
     body= @params[:body]
-    reply_to = @params[:replyTo]
-    if reply_to
-      parent = ChatMessage.find(reply_to)
-      parent.children.create!(body: body, conference_id: conference.id, room_id: room_id, room_type: room_type)
-    else
-      ChatMessage.create!(body: body, conference_id: conference.id, room_id: room_id, room_type: room_type)
-    end
+    ChatMessage.create!(body: body, conference_id: conference.id, talk_id: talk_id)
   end
 end

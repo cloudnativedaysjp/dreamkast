816dea47e56db4e97b27011c171086d5b12a3f45
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-19 22:57:30 +0900

    Revert "foreign_key, index, referenceを個別に消してカラムを追加するように変更"
    This reverts commit 9b69af83978b8aafddf34c411edc3a9d59df13ae.

--- db/migrate/20210216095643_update_columns_to_chat_messages.rb
+++ db/migrate/20210216095643_update_columns_to_chat_messages.rb
@@ -1,14 +1,7 @@
 class UpdateColumnsToChatMessages < ActiveRecord::Migration[6.0]
   def change
-    remove_foreign_key :chat_messages, :talks
-    remove_index :chat_messages, :talk_id
-    remove_reference :chat_messages, :talk, index: true
-
-    remove_foreign_key :chat_messages, :booths
-    remove_index :chat_messages, :booth_id
-    remove_reference :chat_messages, :booth, index: true
-
+    rename_column :chat_messages, :talk_id, :room_id
+    remove_column :chat_messages, :booth_id
     add_column :chat_messages, :room_type, :string
-    add_column :chat_messages, :room_id, :bigint
   end
 end

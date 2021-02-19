9b69af83978b8aafddf34c411edc3a9d59df13ae
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-19 22:48:38 +0900

    foreign_key, index, referenceを個別に消してカラムを追加するように変更
    単純にrenameするだけだと外部キー制約にひっかかっていた。

--- db/migrate/20210216095643_update_columns_to_chat_messages.rb
+++ db/migrate/20210216095643_update_columns_to_chat_messages.rb
@@ -1,7 +1,14 @@
 class UpdateColumnsToChatMessages < ActiveRecord::Migration[6.0]
   def change
-    rename_column :chat_messages, :talk_id, :room_id
-    remove_column :chat_messages, :booth_id
+    remove_foreign_key :chat_messages, :talks
+    remove_index :chat_messages, :talk_id
+    remove_reference :chat_messages, :talk, index: true
+
+    remove_foreign_key :chat_messages, :booths
+    remove_index :chat_messages, :booth_id
+    remove_reference :chat_messages, :booth, index: true
+
     add_column :chat_messages, :room_type, :string
+    add_column :chat_messages, :room_id, :bigint
   end
 end

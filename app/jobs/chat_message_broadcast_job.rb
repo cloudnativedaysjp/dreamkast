aa5301c050383937672ee1e3c8b9dc9eda2004fb
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-27 12:00:44 +0900

    speaker idとprofile idがスネークケースになっていた

--- app/jobs/chat_message_broadcast_job.rb
+++ app/jobs/chat_message_broadcast_job.rb
@@ -6,8 +6,8 @@ class ChatMessageBroadcastJob < ApplicationJob
     channel_name = "chat_channel_#{msg.room_type}#{msg.room_id}"
     ActionCable.server.broadcast channel_name, {
       id: msg.id,
-      profile_id: msg.profile_id,
-      speaker_id: msg.speaker_id,
+      profileId: msg.profile_id,
+      speakerId: msg.speaker_id,
       body: msg.body,
       roomType: msg.room_type,
       roomId: msg.room_id,

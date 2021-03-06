c047e09a0cdc7da7cc39d8ff8154b3cbebab33e4
r_takaishi <ryo.takaishi.0@gmail.com>
2021-03-06 17:06:29 +0900

    ログインしてイベントに登録している場合Profile情報を返すAPIを追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   namespace 'api' do
     namespace 'v1' do
+      get ':event/my_profile' => 'profiles#my_profile'
       resources :conferences, only: [:show], path: "events"
       resources :talks, only: [:index, :show]
       resources :tracks, only: [:index, :show]

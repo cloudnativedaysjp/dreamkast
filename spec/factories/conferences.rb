42c37d81d6459d53d94b1735b3f16b9e3bed63c5
Kazuto Kusama <jacopen@gmail.com>
2021-06-01 00:05:22 +0900

    Hide video entry and form for document url while speaker entry form is enabled

--- spec/factories/conferences.rb
+++ spec/factories/conferences.rb
@@ -76,14 +76,31 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
     id { 2 }
     name { 'CloudNative Days Online 2021'}
     abbr { 'cndo2021' }
-    theme { 'これはCloudNativeDaysOnlinee2021のテーマです' }
-    copyright { '© CloudNativeDaysOnline 2021 Committee' }
+    theme { 'これはTestEventAutumn2020のテーマです' }
+    copyright { '© Test Event Autumn 2020 Committee' }
     privacy_policy { 'This is Privacy Policy' }
     privacy_policy_for_speaker { 'This is Privacy Policy for speaker' }
     status { 2 }
     speaker_entry { 1 }
     attendee_entry { 1 }
     show_timetable { 1 }
+    about { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
+  end
+  factory :cndt2020_speaker_entry_enabled, class: Conference do
+    id { 3 }
+    name { 'CloudNative Days Tokyo 2020'}
+    abbr { 'cndt2020' }
+    theme { 'これはCICD2021のテーマです' }
+    copyright { '© CloudNativeDaysOnline 2021 Committee' }
+    privacy_policy { 'This is Privacy Policy' }
+    privacy_policy_for_speaker { 'This is Privacy Policy for speaker' }
+    status { 0 }
+    speaker_entry { 1 }
+    attendee_entry { 0 }
+    show_timetable { 0 }
     about { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.

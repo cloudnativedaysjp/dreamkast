c67825f47f7c31b722e7ba5eaf9a555813eec4bc
r_takaishi <ryo.takaishi.0@gmail.com>
2021-06-13 12:20:15 +0900

    support updating proposal status

--- app/controllers/admin/proposals_controller.rb
+++ app/controllers/admin/proposals_controller.rb
@@ -12,6 +12,15 @@ class Admin::ProposalsController < ApplicationController
     end
   end
 
+  def update_proposals
+    params[:proposal].each do |proposal_id, value|
+      proposal = Proposal.find(proposal_id)
+      proposal[:status] = value[:status].to_i
+      proposal.save
+    end
+    redirect_to admin_proposals_url, notice: "配信設定を更新しました"
+  end
+
   private
 
   def is_admin?
--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,7 @@ Rails.application.routes.draw do
       post 'close_timetable' => 'timetables#close'
       get 'preview_timetable' => 'timetables#preview'
       put 'talks' => 'talks#update_talks'
+      put 'proposals' => 'proposals#update_proposals'
       post 'bulk_insert_talks' => 'talks#bulk_insert_talks'
       post 'bulk_insert_talks_speaker' => 'talks#bulk_insert_talks_speaker'
       resources :tracks, only: [:index]

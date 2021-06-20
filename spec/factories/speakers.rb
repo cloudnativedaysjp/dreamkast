4cef78ed35ee137b1bb0971c55412ff2abc08742
r_takaishi <ryo.takaishi.0@gmail.com>
2021-06-20 20:07:14 +0900

    add tests

--- spec/factories/speakers.rb
+++ spec/factories/speakers.rb
@@ -9,12 +9,32 @@ FactoryBot.define do
     job_title { 'job_title' }
     conference_id { 1 }
 
-    trait :with_talk1 do
+    trait :with_talk1_registered do
       after(:build) do |speaker|
         talk = FactoryBot.create(:talk1)
         speaker.talks << talk
         FactoryBot.create(:talks_speakers, {talk: talk, speaker: speaker})
-        proposal = FactoryBot.create(:proposal, :with_cndt2021, talk: talk)
+        proposal = FactoryBot.create(:proposal, :with_cndt2021, talk: talk, status: 0)
+        FactoryBot.create(:proposals_speakers, speaker: speaker, proposal: proposal)
+      end
+    end
+
+    trait :with_talk1_accepted do
+      after(:build) do |speaker|
+        talk = FactoryBot.create(:talk1)
+        speaker.talks << talk
+        FactoryBot.create(:talks_speakers, {talk: talk, speaker: speaker})
+        proposal = FactoryBot.create(:proposal, :with_cndt2021, talk: talk, status: 1)
+        FactoryBot.create(:proposals_speakers, speaker: speaker, proposal: proposal)
+      end
+    end
+
+    trait :with_talk1_rejected do
+      after(:build) do |speaker|
+        talk = FactoryBot.create(:talk1)
+        speaker.talks << talk
+        FactoryBot.create(:talks_speakers, {talk: talk, speaker: speaker})
+        proposal = FactoryBot.create(:proposal, :with_cndt2021, talk: talk, status: 2)
         FactoryBot.create(:proposals_speakers, speaker: speaker, proposal: proposal)
       end
     end
--- spec/requests/speaker_dashboard_spec.rb
+++ spec/requests/speaker_dashboard_spec.rb
@@ -3,10 +3,40 @@ require 'rails_helper'
 describe SpeakerDashboardsController, type: :request do
   admin_userinfo = {userinfo: {info: {email: "alice@example.com"}, extra: {raw_info: {sub: "aaaa", "https://cloudnativedays.jp/roles" => ["CNDT2020-Admin"]}}}}
   describe "GET speaker_dashboards#show" do
-    context 'CNDT2020 is registered' do
-      before do
-        create(:cndt2020, :registered)
+
+    shared_examples_for :request_is_successful do
+      it "request is successful" do
+        get '/cndt2020/speaker_dashboard'
+        expect(response).to be_successful
+        expect(response).to have_http_status '200'
       end
+    end
+
+    shared_examples_for :response_includes_proposal_title_and_entry_status do |title, entry_status|
+      it "include information about proposal" do
+        get '/cndt2020/speaker_dashboard'
+        expect(response.body).to include title
+        expect(response.body).to include entry_status
+      end
+    end
+
+    shared_examples_for :response_does_not_include_proposal_title_and_entry_status do |title, entry_status|
+      it "include information about proposal" do
+        get '/cndt2020/speaker_dashboard'
+        expect(response.body).to_not include title
+        expect(response.body).to_not include entry_status
+      end
+    end
+
+    shared_examples_for :response_includes_edit_button do
+      it "include edit button " do
+        get '/cndt2020/speaker_dashboard'
+        expect(response.body).to include 'edit'
+      end
+    end
+
+    context 'CNDT2020 is registered' do
+      let!(:cndt2020) { create(:cndt2020, :registered) }
 
       describe "speaker doesn't logged in" do
         it "returns a success response with speaker dashboard page" do
@@ -37,13 +67,12 @@ describe SpeakerDashboardsController, type: :request do
             create(:speaker_alice)
           end
 
-          it "returns a success response with event top page" do
+          it "response includes header text" do
             get '/cndt2020/speaker_dashboard'
-            expect(response).to be_successful
-            expect(response).to have_http_status '200'
             expect(response.body).to include 'スピーカーダッシュボード'
-            expect(response.body).to include 'edit'
           end
+          it_should_behave_like :request_is_successful
+          it_should_behave_like :response_includes_edit_button
         end
       end
     end
@@ -51,16 +80,98 @@ describe SpeakerDashboardsController, type: :request do
     context 'CNDT2020 is registered and speaker entry is enabled' do
       before do
         allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(admin_userinfo)
-        create(:cndt2020, :registered, :speaker_entry_enabled)
-        create(:speaker_alice)
       end
 
-      it "doesn't include information about video status" do
-        get '/cndt2020/speaker_dashboard'
-        expect(response).to be_successful
-        expect(response).to have_http_status '200'
-        expect(response.body).to_not include 'ビデオファイル提出状況'
-        expect(response.body).to include 'edit'
+      context 'CFP result is visible' do
+        before do
+          create(:cndt2020, :registered, :speaker_entry_enabled, :cfp_result_visible)
+        end
+
+        context "speaker doesn't have proposal" do
+          before do
+            create(:speaker_alice)
+          end
+
+          it_should_behave_like :request_is_successful
+          it_should_behave_like :response_does_not_include_proposal_title_and_entry_status, 'talk1', '受付状況'
+          it_should_behave_like :response_includes_edit_button
+        end
+
+        context "speaker has proposal and it is registered" do
+          before do
+            create(:speaker_alice, :with_talk1_registered)
+          end
+
+          it_should_behave_like :request_is_successful
+          it_should_behave_like :response_includes_proposal_title_and_entry_status, 'talk1', '受付状況: エントリー済み'
+          it_should_behave_like :response_includes_edit_button
+        end
+
+        context "speaker has proposal and it is accepted" do
+          before do
+            create(:speaker_alice, :with_talk1_accepted)
+          end
+
+          it_should_behave_like :request_is_successful
+          it_should_behave_like :response_includes_proposal_title_and_entry_status, 'talk1', '受付状況: 採択'
+          it_should_behave_like :response_includes_edit_button
+        end
+
+        context "speaker has proposal and it is rejected" do
+          before do
+            create(:speaker_alice, :with_talk1_rejected)
+          end
+
+          it_should_behave_like :request_is_successful
+          it_should_behave_like :response_includes_proposal_title_and_entry_status, 'talk1', '受付状況: 不採択'
+          it_should_behave_like :response_includes_edit_button
+        end
+      end
+
+      context 'CFP result is invisible' do
+        before do
+          create(:cndt2020, :registered, :speaker_entry_enabled, :cfp_result_invisible)
+        end
+
+        context "speaker doesn't have proposal" do
+          before do
+            create(:speaker_alice)
+          end
+
+          it_should_behave_like :request_is_successful
+          it_should_behave_like :response_does_not_include_proposal_title_and_entry_status, 'talk1', '受付状況'
+          it_should_behave_like :response_includes_edit_button
+        end
+
+        context "speaker has proposal and it is registered" do
+          before do
+            create(:speaker_alice, :with_talk1_registered)
+          end
+
+          it_should_behave_like :request_is_successful
+          it_should_behave_like :response_includes_proposal_title_and_entry_status, 'talk1', '受付状況: エントリー済み'
+          it_should_behave_like :response_includes_edit_button
+        end
+
+        context "speaker has proposal and it is accepted" do
+          before do
+            create(:speaker_alice, :with_talk1_accepted)
+          end
+
+          it_should_behave_like :request_is_successful
+          it_should_behave_like :response_includes_proposal_title_and_entry_status, 'talk1', '受付状況: エントリー済み'
+          it_should_behave_like :response_includes_edit_button
+        end
+
+        context "speaker has proposal and it is rejected" do
+          before do
+            create(:speaker_alice, :with_talk1_rejected)
+          end
+
+          it_should_behave_like :request_is_successful
+          it_should_behave_like :response_includes_proposal_title_and_entry_status, 'talk1', '受付状況: エントリー済み'
+          it_should_behave_like :response_includes_edit_button
+        end
       end
     end
   end

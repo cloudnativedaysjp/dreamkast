7b62f6beac93ef624fe7b0f256eccd4a2ff8f31f
YHojo <hojo.yosuke@gmail.com>
2021-07-03 21:25:50 +0900

    非同期の設定を追加
    - production を設定
    - ActiveRecord に起因する例外の際にJobを破棄する
    - 既存のメール処理を非同期に変更

--- app/jobs/mail_delivery_job.rb
+++ app/jobs/mail_delivery_job.rb
@@ -2,6 +2,8 @@ class MailDeliveryJob < ApplicationJob
   queue_as :mail
   self.queue_adapter = :amazon_sqs
 
+  discard_on ActiveRecordError
+
   def perform(mailer, mail_method, delivery_method, *args)
     mailer.constantize.public_send(mail_method, *args).send(delivery_method)
   end

85de9659404784bd82c2f5b33d013c83806c8767
r_takaishi <ryo.takaishi.0@gmail.com>
2021-07-31 20:39:00 +0900

    add seeds for prod

---
+++ db/fixtures/production/01_proposal_item_configs.rb
@@ -0,0 +1,251 @@
+ProposalItemConfig.seed(
+  {
+    id: 1,
+    conference_id: 3,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'architect - システム設計'
+  },
+  {
+    id: 2,
+    conference_id: 3,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'developer - システム開発'
+  },
+  {
+    id: 3,
+    conference_id: 3,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'app-developer - アプリケーション開発'
+  },
+  {
+    id: 4,
+    conference_id: 3,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'operator/sys-admin - 運用管理/システム管理'
+  },
+  {
+    id: 5,
+    conference_id: 3,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'CxO/biz - ビジネス層'
+  },
+  {
+    id: 6,
+    conference_id: 3,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'その他'
+  },
+  {
+    id: 7,
+    conference_id: 3,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'execution_phase',
+    item_number: 5,
+    item_name: '実行フェーズ（★★）',
+    params: 'Dev/QA（開発環境）'
+  },
+  {
+    id: 8,
+    conference_id: 3,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'execution_phase',
+    item_number: 5,
+    item_name: '実行フェーズ（★★）',
+    params: 'PoC（検証）'
+  },
+  {
+    id: 9,
+    conference_id: 3,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'execution_phase',
+    item_number: 5,
+    item_name: '実行フェーズ（★★）',
+    params: 'Production（本番環境）'
+  },
+
+  # =========== CICD2021 =============
+  {
+    id: 10,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'architect - システム設計'
+  },
+  {
+    id: 11,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'developer - システム開発'
+  },
+  {
+    id: 12,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'app-developer - アプリケーション開発'
+  },
+  {
+    id: 13,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'operator/sys-admin - 運用管理/システム管理'
+  },
+  {
+    id: 14,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'CxO/biz - ビジネス層'
+  },
+  {
+    id: 15,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'assumed_visitor',
+    item_number: 1,
+    item_name: '想定受講者（★★）',
+    params: 'その他'
+  },
+
+  {
+    id: 16,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'execution_phase',
+    item_number: 2,
+    item_name: '実行フェーズ（★★）',
+    params: 'Dev/QA（開発環境）'
+  },
+  {
+    id: 17,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'execution_phase',
+    item_number: 2,
+    item_name: '実行フェーズ（★★）',
+    params: 'PoC（検証）'
+  },
+  {
+    id: 18,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'execution_phase',
+    item_number: 2,
+    item_name: '実行フェーズ（★★）',
+    params: 'Production（本番環境）'
+  },
+  {
+    id: 19,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'execution_phase',
+    item_number: 2,
+    item_name: '実行フェーズ（★★）',
+    params: 'Other'
+  },
+
+  {
+    id: 20,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'whether_it_can_be_published',
+    item_number: 3,
+    item_name: 'スライドと動画の公開可否（★★）',
+    params: 'All okay - スライド・動画両方ともに公開可'
+  },
+  {
+    id: 21,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'whether_it_can_be_published',
+    item_number: 3,
+    item_name: 'スライドと動画の公開可否（★★）',
+    params: 'Only Slide - スライドのみ公開可'
+  },
+  {
+    id: 22,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'whether_it_can_be_published',
+    item_number: 3,
+    item_name: 'スライドと動画の公開可否（★★）',
+    params: 'NG - いずれも公開不可（来場者限定のコンテンツ）'
+  },
+  {
+    id: 23,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'whether_it_can_be_published',
+    item_number: 3,
+    item_name: 'スライドと動画の公開可否（★★）',
+    params: 'その他'
+  },
+
+  {
+    id: 24,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'session_time',
+    item_number: 4,
+    item_name: '必要とする講演時間 - Session time you need（★）',
+    params: '40min (full session)'
+  },
+  {
+    id: 25,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'session_time',
+    item_number: 4,
+    item_name: '必要とする講演時間 - Session time you need（★）',
+    params: '20min (half session)'
+  },
+
+  {
+    id: 26,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'language',
+    item_number: 5,
+    item_name: '講演言語 - Language（★）',
+    params: 'JA'
+  },
+  {
+    id: 27,
+    conference_id: 4,
+    type: 'ProposalItemConfigCheckBox',
+    label: 'language',
+    item_number: 5,
+    item_name: '講演言語 - Language（★）',
+    params: 'EN'
+  }
+)

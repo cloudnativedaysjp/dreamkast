9cdeae4fef98821dd0bae286ad51c64f030b6ac3
r_takaishi <ryo.takaishi.0@gmail.com>
2021-08-11 18:21:01 +0900

    add twitter id and remove url
    url isn't supported

--- app/controllers/admin/talks_controller.rb
+++ app/controllers/admin/talks_controller.rb
@@ -13,7 +13,7 @@ class Admin::TalksController < ApplicationController
         head :no_content
 
         filename = "#{@conference.abbr}_talks"
-        columns = %w[id title abstract speaker session_time difficulty category created_at url]
+        columns = %w[id title abstract speaker session_time difficulty category created_at twitter_id]
         labels = @conference.proposal_item_configs.map(&:label).uniq
         labels.delete('session_time')
         columns.concat(labels)
@@ -23,7 +23,7 @@ class Admin::TalksController < ApplicationController
           csv << columns
 
           @talks.each do |talk|
-            row = [talk.id, talk.title, talk.abstract, talk.speaker_names.join(", "), talk.time, talk.talk_difficulty.name, talk.talk_category.name, talk.created_at, talk.url]
+            row = [talk.id, talk.title, talk.abstract, talk.speaker_names.join(", "), talk.time, talk.talk_difficulty.name, talk.talk_category.name, talk.created_at, talk.speaker_twitter_ids.join(", ")]
             labels.each do |label|
               v = talk.proposal_item_value(label)
               row << v.class == Array ? v.join(', ') : v

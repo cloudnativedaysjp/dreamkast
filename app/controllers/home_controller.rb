dd901e49e48eaa6da1443f77c8b6808f7747ae73
Kazuto Kusama <jacopen@gmail.com>
2021-08-29 12:17:38 +0900

    Move scope to model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   include HomeHelper
   def show
-    @upcoming = Conference.merge(Conference.where(status: 0).or(Conference.where(status: 1)))
-    @archived = Conference.merge(Conference.where(status: 2).or(Conference.where(status: 3)))
+    @upcoming = Conference.upcoming
+    @previous = Conference.previous
   end
 end

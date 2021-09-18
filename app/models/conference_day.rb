45fbd79b9b4131b8895758c5202f1ef8fe1d1c32
r_takaishi <ryo.takaishi.0@gmail.com>
2021-09-18 10:00:07 +0900

    refactor: remove 1 condition block

--- app/models/conference_day.rb
+++ app/models/conference_day.rb
@@ -1,3 +1,7 @@
 class ConferenceDay < ApplicationRecord
   has_many :talks, -> {order('conference_day_id ASC, HOUR(start_time) ASC, track_id ASC, MINUTE(start_time) ASC')}
+
+  scope :externals, -> {
+    where(internal: false)
+  }
 end

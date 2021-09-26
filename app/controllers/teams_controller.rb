d1d7c6317f7f7395002a8fe55092e6ab1fc8ec20
YHojo <hojo.yosuke@gmail.com>
2021-09-26 23:05:12 +0900

    fix problem team page not to show current user

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,13 @@
 class TeamsController < ApplicationController
-
+  include Secured
   before_action :set_conference
+
+  def logged_in_using_omniauth?
+    if session[:userinfo].present?
+      @current_user = session[:userinfo]
+    end
+  end
+
   def show
     @admin_profiles = @conference.admin_profiles.where(show_on_team_page: true).order(name: 'ASC')
   end

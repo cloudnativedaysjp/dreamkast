593a7430f78f6b8ccb0e54d70f85c3d8318edae5
r_takaishi <ryo.takaishi.0@gmail.com>
2021-10-10 18:41:45 +0900

    use instance profile when environment variables isn't defined

--- app/models/live_stream_ivs.rb
+++ app/models/live_stream_ivs.rb
@@ -49,7 +49,11 @@ class LiveStreamIvs < LiveStream
 
   def ivs_client
     creds = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-    Aws::IVS::Client.new(region: 'us-east-1', credentials: creds)
+    if ENV['AWS_ACCESS_KEY_ID']
+      Aws::IVS::Client.new(region: 'us-east-1', credentials: creds)
+    else
+      Aws::IVS::Client.new(region: 'us-east-1')
+    end
   end
 
   def env_name_for_tag

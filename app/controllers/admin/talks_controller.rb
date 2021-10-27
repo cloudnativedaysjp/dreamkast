a81065035f70cd0d42aa4fb63bc5def166145b44
r_takaishi <ryo.takaishi.0@gmail.com>
2021-10-27 18:48:02 +0900

    improve operation on_air status

--- app/controllers/admin/talks_controller.rb
+++ app/controllers/admin/talks_controller.rb
@@ -22,6 +22,55 @@ class Admin::TalksController < ApplicationController
     redirect_to admin_talks_url, notice: "配信設定を更新しました"
   end
 
+  def start_on_air
+    talk = Talk.find( params[:talk][:id])
+
+    talk.conference_day
+    talk.track
+    if @conference.tracks.find_by(name: talk.track.name).talks
+                  .select{|t| t.conference_day.id == talk.conference_day.id && t.id != talk.id}
+                  .map(&:video)
+                  .map(&:on_air)
+                  .any?
+      flash[:danger] = "他にOnAir中のセッションがあります。"
+      redirect_to admin_tracks_path
+    else
+
+
+
+    talk.conference_day
+    video = talk.video
+    video.on_air = true
+    video.save!
+
+    ActionCable.server.broadcast(
+      "track_channel", Video.on_air(conference)
+    )
+    ActionCable.server.broadcast(
+      "on_air_#{conference.abbr}", Video.on_air_v2
+    )
+
+    flash[:notice] = "配信設定を更新しました"
+    redirect_to admin_tracks_path
+    end
+  end
+
+  def stop_on_air
+    talk = Talk.find(params[:talk][:id])
+    video = talk.video
+    video.on_air = false
+    video.save!
+
+    ActionCable.server.broadcast(
+      "track_channel", Video.on_air(conference)
+    )
+    ActionCable.server.broadcast(
+      "on_air_#{conference.abbr}", Video.on_air_v2
+    )
+
+    redirect_to admin_tracks_path, notice: "配信設定を更新しました"
+  end
+
   def bulk_insert_talks
     unless params[:file]
       redirect_to '/admin/talks', notice: "アップロードするファイルを選択してください"

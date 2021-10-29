f25d58cf67b3384b1b48786c5d8473c0ff424915
r_takaishi <ryo.takaishi.0@gmail.com>
2021-10-30 01:13:31 +0900

    add log

--- app/javascript/packs/admin/recording/recording_channel.js
+++ app/javascript/packs/admin/recording/recording_channel.js
@@ -2,14 +2,16 @@ import consumer from "./consumer"
 
 consumer.subscriptions.create("RecordingChannel", {
   connected() {
+    console.log('Connected: RecordingChannel')
   },
 
   disconnected() {
+    console.log('Disconnected: RecordingChannel')
     // Called when the subscription has been terminated by the server
   },
 
   received(data) {
-    console.log(data)
+    console.log('Received: ' + JSON.stringify(data))
     window.location.reload()
   },
 

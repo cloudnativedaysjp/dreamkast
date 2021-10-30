c428f5282c798485e4658cc91a3e65c2fa7e37d8
r_takaishi <ryo.takaishi.0@gmail.com>
2021-10-30 20:46:25 +0900

    ActionCableを使うのを止めたので戻す

--- app/channels/recording_channel.rb
+++
@@ -1,9 +0,0 @@
-class RecordingChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "recording_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end

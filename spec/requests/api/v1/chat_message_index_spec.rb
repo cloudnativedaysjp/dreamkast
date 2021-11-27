326b5d4b0ec03f191b2b1da62359f42c0c332e25
r_takaishi <ryo.takaishi.0@gmail.com>
2021-11-27 12:02:33 +0900

    rubocop auto collect

--- spec/requests/api/v1/chat_message_index_spec.rb
+++ spec/requests/api/v1/chat_message_index_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe Api::V1::ChatMessagesController, type: :request do
   subject(:session) {
@@ -6,7 +6,7 @@ describe Api::V1::ChatMessagesController, type: :request do
       userinfo: {
         info: {
           email: "alice@example.com",
-          extra: {sub: "alice"}
+          extra: { sub: "alice" }
         },
         extra: {
           raw_info: {
@@ -18,24 +18,24 @@ describe Api::V1::ChatMessagesController, type: :request do
     }
   }
 
-  describe 'GET /api/v1/chat_messages' do
+  describe "GET /api/v1/chat_messages" do
     before do
-      allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(userinfo: {info: {email: alice.email}})
+      allow_any_instance_of(ActionDispatch::Request).to(receive(:session).and_return(userinfo: { info: { email: alice.email } }))
       create(:talk1)
       create(:message_from_alice, profile: alice)
     end
 
     let!(:cndt2020) { create(:cndt2020) }
-    let!(:alice) { create(:alice, :on_cndt2020, conference: cndt2020)}
+    let!(:alice) { create(:alice, :on_cndt2020, conference: cndt2020) }
 
-    it 'succeed request' do
-      get '/api/v1/chat_messages?eventAbbr=cndt2020&roomId=1&roomType=talk'
-      expect(response).to have_http_status :ok
-      expect(response.status).to eq 200
+    it "succeed request" do
+      get "/api/v1/chat_messages?eventAbbr=cndt2020&roomId=1&roomType=talk"
+      expect(response).to(have_http_status(:ok))
+      expect(response.status).to(eq(200))
     end
 
-    it 'confirm json schema' do
-      get '/api/v1/chat_messages?eventAbbr=cndt2020&roomId=1&roomType=talk'
+    it "confirm json schema" do
+      get "/api/v1/chat_messages?eventAbbr=cndt2020&roomId=1&roomType=talk"
       assert_response_schema_confirm
     end
   end

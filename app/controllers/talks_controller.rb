12147e11c33ac20e3e99b2c6246c89fe6e6e8dba
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2021-12-11 19:05:20 +0900

    videoを表示するかどうかの判定を全てヘルパーメソッドに集約

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -27,6 +27,8 @@ class TalksController < ApplicationController
 
   private
 
+  helper_method :display_video?
+
   def talk_params
     params.require(:talk).permit(:title, :abstract, :movie_url, :track, :start_time, :end_time, :talk_difficulty_id, :talk_category_id)
   end
@@ -42,6 +44,12 @@ class TalksController < ApplicationController
       talk.start_time.strftime("%H:%M") + "-" + talk.end_time.strftime("%H:%M")
     else
       ""
+    end
+  end
+
+  def display_video?(conference, talk)
+    if (conference.closed? && @current_user) || (conference.opened? && @current_user) || conference.archived?
+      talk.video_published && talk.video.present? && talk.archived?
     end
   end
 end

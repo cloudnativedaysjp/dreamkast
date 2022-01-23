db7a2a4593ff057424fecb5993e6efb2255311be
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2022-01-23 16:57:29 +0900

    record archive video by harvest job

---
+++ app/controllers/admin/harvest_jobs_controller.rb
@@ -0,0 +1,41 @@
+class Admin::HarvestJobsController < ApplicationController
+  include SecuredAdmin
+  include LogoutHelper
+
+  def index
+    @harvest_jobs = @conference.media_package_harvest_jobs
+  end
+
+  def new
+    @harvest_job = MediaPackageHarvestJob.new
+    @talk = Talk.find(params[:talk_id])
+
+    @initial_start_time = "#{initial_date}T#{@talk.start_time.strftime('%H:%M')}:00+09:00"
+    @initial_end_time = "#{initial_date}T#{@talk.end_time.strftime('%H:%M')}:00+09:00"
+    @base_url = @talk.track.media_package_channel.media_package_origin_endpoints.first.origin_endpoint.url
+    @preview_url = "#{@base_url}?start=#{@initial_start_time}&end=#{@initial_end_time}"
+  end
+
+  def create
+    @job = MediaPackageHarvestJob.new(harvest_job_params.merge(conference_id: @conference.id))
+
+    respond_to do |format|
+      if @job.save
+        @job.create_media_package_resources
+        format.html { redirect_to(admin_tracks_path, notice: 'HarvestJob was successfully updated.') }
+        format.json { render(:show, status: :ok, location: @job) }
+      else
+        format.html { redirect_to(admin_tracks_path, flash: { error: @job.errors.messages }) }
+        format.json { render(json: @job.errors, status: :unprocessable_entity) }
+      end
+    end
+  end
+
+  def initial_date
+    @talk.conference_day.date.strftime('%Y-%m-%d')
+  end
+
+  def harvest_job_params
+    params.require(:media_package_harvest_job).permit(:conference_id, :media_package_channel_id, :talk_id, :start_time, :end_time)
+  end
+end
---
+++ app/models/media_package_harvest_job.rb
@@ -0,0 +1,78 @@
+# == Schema Information
+#
+# Table name: media_package_harvest_jobs
+#
+#  id                       :bigint           not null, primary key
+#  end_time                 :datetime
+#  start_time               :datetime
+#  status                   :string(255)
+#  conference_id            :bigint           not null
+#  job_id                   :string(255)
+#  media_package_channel_id :bigint           not null
+#  talk_id                  :bigint           not null
+#
+# Indexes
+#
+#  index_media_package_harvest_jobs_on_conference_id             (conference_id)
+#  index_media_package_harvest_jobs_on_media_package_channel_id  (media_package_channel_id)
+#  index_media_package_harvest_jobs_on_talk_id                   (talk_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (conference_id => conferences.id)
+#  fk_rails_...  (media_package_channel_id => media_package_channels.id)
+#  fk_rails_...  (talk_id => talks.id)
+#
+class MediaPackageHarvestJob < ApplicationRecord
+  include MediaPackageHelper
+  include EnvHelper
+
+  belongs_to :conference
+  belongs_to :media_package_channel
+  belongs_to :talk
+
+  def job
+    @job ||= media_package_client.describe_harvest_job(id: job_id)
+  end
+
+  def create_media_package_resources
+    resp = media_package_client.create_harvest_job(create_params)
+    resp = media_package_client.describe_harvest_job(id: resp.id)
+    update!(job_id: resp.id, status: resp.status)
+  rescue => e
+    logger.error(e.message)
+  end
+
+  private
+
+  def create_params
+    {
+      id: "#{resource_name}_#{id}",
+      start_time: start_time.strftime('%Y-%m-%dT%H:%M:%S%:z'),
+      end_time: end_time.strftime('%Y-%m-%dT%H:%M:%S%:z'),
+      origin_endpoint_id: origin_endpoint,
+      s3_destination: {
+        bucket_name: bucket_name,
+        manifest_key: manifest_key,
+        role_arn: 'arn:aws:iam::607167088920:role/MediaPackageLivetoVOD-Policy'
+      }
+    }
+  end
+
+  def origin_endpoint
+    "#{env_name}_#{@job.conference.abbr}_track#{@job.talk.track.name}"
+  end
+
+  def manifest_key
+    "mediapackage/#{conference.abbr}/talks/#{talk_id}/playlist.m3u8"
+  end
+
+  def resource_name
+    track = media_package_channel.track
+    if review_app?
+      "review_app_#{review_app_number}_#{conference.abbr}_track#{track.name}"
+    else
+      "#{env_name}_#{conference.abbr}_track#{track.name}"
+    end
+  end
+end

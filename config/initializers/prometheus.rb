311fcb60bc285f900b6d5e7adb144c2caa79c0e0
machih <machih@vmware.com>
2022-02-09 10:07:40 +0900

    Fix prometheus logic

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -6,8 +6,17 @@ module Prometheus
   module Controller
     metrics_prefix = 'dreamkast'
     prometheus = Prometheus::Client.registry
-    VIEWER_COUNT = Prometheus::Client::Gauge.new("#{metrics_prefix}_viewer_count".to_sym, docstring: "Count #{metrics_prefix} viewer number", labels: [:track_id])
-    CHAT_COUNT = Prometheus::Client::Gauge.new("#{metrics_prefix}_chat_count".to_sym, docstring: "Count #{metrics_prefix} chat number", labels: [:room_id])
+    VIEWER_COUNT = Prometheus::Client::Gauge.new(
+      "#{metrics_prefix}_viewer_count".to_sym,
+      docstring: "Count #{metrics_prefix} viewer number",
+      labels: [:talk_id, :track_id, :conference_id]
+    )
+
+    CHAT_COUNT = Prometheus::Client::Gauge.new(
+      "#{metrics_prefix}_chat_count".to_sym,
+      docstring: "Count #{metrics_prefix} chat number",
+      labels: [:conference_id, :talk_id]
+    )
 
     prometheus.register(VIEWER_COUNT)
     prometheus.register(CHAT_COUNT)
@@ -16,25 +25,37 @@ module Prometheus
   module Middleware
     class CustomExporter < Prometheus::Middleware::Exporter
       def respond_with(format)
-        conferences = Conference.all
-        puts(conferences)
-        conferences.each do |conference|
-          tracks = Track.where(conference_id: conference.id)
-          tracks.each do |track|
-            vc = ViewerCount.where(track_id: track.id).order(created_at: :desc).limit(1)[0]
-            puts(vc.to_s)
-            unless vc.nil?
-              Prometheus::Controller::VIEWER_COUNT.set(vc.count, labels: { track_id: track.id })
-            end
-          end
-          chat_counts = ChatMessage.where(conference_id: conference.id).group('room_id').count
-
-          chat_counts.each do |chat_count|
-            Prometheus::Controller::CHAT_COUNT.set(chat_count.count_all, labels: { room_id: chat_count.chat_messages_room_id })
-          end
-        end
-
+        calculate_viewer_count
+        calculate_chat_count
         super
+      end
+
+      private
+
+      def calculate_viewer_count
+        vcs = ViewerCount.all
+        vcs.each do |vc|
+          Prometheus::Controller::VIEWER_COUNT.set(
+            vc.count,
+            labels: { talk_id: vc.talk_id, track_id: vc.track_id, conference_id: vc.conference_id }
+          )
+        end
+      end
+
+      def calculate_chat_count
+        chat_counts = ChatMessage.all.select(
+          [
+            :conference_id, :speaker_id, ChatMessage.arel_table[:speaker_id].count.as('count')
+          ]
+        ).group(:conference_id, :speaker_id)
+
+        chat_counts.each do |chat_count|
+          talk_id = TalksSpeaker.where(speaker_id: chat_count.speaker_id).limit(1)[0].talk_id
+          Prometheus::Controller::CHAT_COUNT.set(
+            chat_count.count,
+            labels: { conference_id: chat_count.conference_id, talk_id: talk_id }
+          )
+        end
       end
     end
   end

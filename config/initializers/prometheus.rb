655de7b15b4c0ca8ef314c4716704efd3d0472d0
machih <machih@vmware.com>
2022-02-10 23:36:19 +0900

    Update prometheus

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -33,7 +33,15 @@ module Prometheus
       private
 
       def calculate_viewer_count
-        vcs = ViewerCount.all
+        accepted_talks = Talk.accepted.pluck(:id)
+
+
+        vcs = ViewerCount.select(
+          [
+            :conference_id, :track_id, :talk_id, ViewerCount.arel_table[:count].maximum.as('count')
+          ]
+        ).where(talk_id: accepted_talks).group(:conference_id, :track_id, :talk_id)
+
         vcs.each do |vc|
           Prometheus::Controller::VIEWER_COUNT.set(
             vc.count,
@@ -45,15 +53,14 @@ module Prometheus
       def calculate_chat_count
         chat_counts = ChatMessage.all.select(
           [
-            :conference_id, :speaker_id, ChatMessage.arel_table[:speaker_id].count.as('count')
+            :conference_id, :room_id, ChatMessage.arel_table[:room_id].count.as('count')
           ]
-        ).group(:conference_id, :speaker_id)
+        ).group(:conference_id, :room_id)
 
         chat_counts.each do |chat_count|
-          talk_id = TalksSpeaker.where(speaker_id: chat_count.speaker_id).limit(1)[0].talk_id
           Prometheus::Controller::CHAT_COUNT.set(
             chat_count.count,
-            labels: { conference_id: chat_count.conference_id, talk_id: talk_id }
+            labels: { conference_id: chat_count.conference_id, talk_id: chat_count.room_id }
           )
         end
       end

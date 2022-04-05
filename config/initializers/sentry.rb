0f8ef50c09e782d501b14ac97b7bde1132cf8036
inductor <kela@inductor.me>
2022-04-05 13:42:17 +0900

    remove unnecessary option

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,9 +7,4 @@ Sentry.init do |config|
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
   config.traces_sample_rate = 0.5
-  # or
-  config.traces_sampler = lambda do |context|
-    true
-  end
-
 end

fc2f2a5051aa5941d43482272725091b81b74b45
inductor <kela@inductor.me>
2022-04-05 13:13:36 +0900

    Enable performance monitoring

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,4 +3,13 @@ Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
   config.environment = ENV['DREAMKAST_NAMESPACE']
   config.enabled_environments = ['dreamkast', 'dreamkast-staging'] # Only staging and production send error logs to sentry
+
+  # To activate performance monitoring, set one of these options.
+  # We recommend adjusting the value in production:
+  config.traces_sample_rate = 0.5
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+
 end

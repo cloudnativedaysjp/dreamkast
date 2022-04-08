0561c2808711ed0662c3e3e245be23eca45d3335
inductor <kela@inductor.me>
2022-04-08 21:56:31 +0900

    stop sending performance metrics to sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,5 +6,5 @@ Sentry.init do |config|
 
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
-  config.traces_sample_rate = 0.5
+  # config.traces_sample_rate = 0.5
 end

2964d3d889b74d54f1d028e742bacb9885c9b78e
YHojo <hojo.yosuke@gmail.com>
2022-06-04 13:17:48 +0900

    2重処理の際のブロックを追加

--- app/jobs/promote_job.rb
+++ app/jobs/promote_job.rb
@@ -1,6 +1,7 @@
 class PromoteJob < ApplicationJob
   def perform(record, name, file_data)
     attacher = Shrine::Attacher.retrieve(model: record, name: name, file: file_data)
+    return unless attacher.file.exists?
     attacher.create_derivatives
     attacher.atomic_promote
   end

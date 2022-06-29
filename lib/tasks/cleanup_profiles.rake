0eacd7e117646d27d13e9dffca4403ed3f9d99e4
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2022-06-29 17:38:55 +0900

    add task to delete profiles

---
+++ lib/tasks/cleanup_profiles.rake
@@ -0,0 +1,20 @@
+namespace :util do
+  desc 'cleanup_profiles'
+  task cleanup_profiles: :environment do
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+    Rails.logger.level = Logger::DEBUG
+
+    abbr = ENV.fetch("EVENT_ABBR")
+
+    ActiveRecord::Base.transaction do
+      begin
+        conference = Conference.find_by(abbr: abbr)
+        conference.profiles.each do |profile|
+          profile.destroy!
+        end
+      rescue => e
+        puts(e)
+      end
+    end
+  end
+end
---
+++ spec/rake/cleanup_profiles_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+require 'rake'
+
+describe 'cleanup_profiles' do
+  before(:all) do
+    @rake = Rake::Application.new
+    Rake.application = @rake
+    Rake.application.rake_require('cleanup_profiles' , ["#{Rails.root}/lib/tasks"])
+    Rake::Task.define_task(:environment)
+    ENV["EVENT_ABBR"] = "cndt2020"
+  end
+
+  before(:each) do
+    @rake[task].reenable
+  end
+
+  let!(:cndt2020) { create(:cndt2020, :opened) }
+  let!(:alice) { create(:alice, :on_cndt2020, conference: cndt2020) }
+  let(:task) { 'util:cleanup_profiles' }
+
+  it 'delete profiles related of conference' do
+    @rake[task].invoke
+    expect(Profile.where(conference_id: cndt2020.id).size).to(eq(0))
+  end
+end

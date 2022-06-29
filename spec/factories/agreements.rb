f878eeb12b585fce1870269732928f9353235db5
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2022-06-29 18:04:31 +0900

    delete agreements

---
+++ spec/factories/agreements.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: agreements
+#
+#  id           :integer          not null, primary key
+#  profile_id   :integer
+#  form_item_id :integer
+#  value        :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+FactoryBot.define do
+  factory :agreement, class: Agreement do
+  end
+end
--- spec/rake/cleanup_profiles_spec.rb
+++ spec/rake/cleanup_profiles_spec.rb
@@ -19,6 +19,8 @@ describe 'cleanup_profiles' do
   let!(:access_log) { create(:access_log, profile: alice) }
   let!(:talk) { create(:talk1) }
   let!(:registered_talk) { create(:registered_talk, profile: alice, talk: talk) }
+  let!(:form_item) { create(:form_item1)}
+  let!(:agreement) { create(:agreement, profile: alice, form_item_id: form_item.id) }
   let!(:chat_message) { create(:message_from_alice, conference_id: cndt2020.id, profile: alice, room_id: talk.id)}
   let(:task) { 'util:cleanup_profiles' }
 
@@ -35,6 +37,11 @@ describe 'cleanup_profiles' do
   it 'delete registered talks related of profile' do
     @rake[task].invoke
     expect(RegisteredTalk.where(profile_id: alice.id).size).to(eq(0))
+  end
+
+  it 'delete agreements related of profile' do
+    @rake[task].invoke
+    expect(Agreement.where(profile_id: alice.id).size).to(eq(0))
   end
 
   it 'doesn\'t delete chat messages related of profile' do

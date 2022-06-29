6679446d4e406c02d352f06a525d0fc5a14f3e1a
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2022-06-29 18:11:34 +0900

    work for archived conference only

--- lib/tasks/cleanup_profiles.rake
+++ lib/tasks/cleanup_profiles.rake
@@ -7,25 +7,24 @@ namespace :util do
     abbr = ENV.fetch("EVENT_ABBR")
 
     ActiveRecord::Base.transaction do
-      begin
-        conference = Conference.find_by(abbr: abbr)
-        conference.profiles.each do |profile|
-          AccessLog.where(profile_id: profile.id).each do |access_log|
-            access_log.destroy!
-          end
-          RegisteredTalk.where(profile_id: profile.id).each do |registered_talk|
-            registered_talk.destroy!
-          end
-          Agreement.where(profile_id: profile.id).each do |agreement|
-            agreement.destroy!
-          end
-          ChatMessage.where(profile_id: profile.id).each do |chat_message|
-            chat_message.update!(profile_id: nil)
-          end
-          profile.destroy!
+      conference = Conference.find_by(abbr: abbr)
+      unless conference.archived?
+        raise "#{conference.abbr} is not archived yet"
+      end
+      conference.profiles.each do |profile|
+        AccessLog.where(profile_id: profile.id).each do |access_log|
+          access_log.destroy!
         end
-      rescue => e
-        puts(e)
+        RegisteredTalk.where(profile_id: profile.id).each do |registered_talk|
+          registered_talk.destroy!
+        end
+        Agreement.where(profile_id: profile.id).each do |agreement|
+          agreement.destroy!
+        end
+        ChatMessage.where(profile_id: profile.id).each do |chat_message|
+          chat_message.update!(profile_id: nil)
+        end
+        profile.destroy!
       end
     end
   end
--- spec/rake/cleanup_profiles_spec.rb
+++ spec/rake/cleanup_profiles_spec.rb
@@ -14,39 +14,67 @@ describe 'cleanup_profiles' do
     @rake[task].reenable
   end
 
-  let!(:cndt2020) { create(:cndt2020, :opened) }
-  let!(:alice) { create(:alice, :on_cndt2020, conference: cndt2020) }
-  let!(:access_log) { create(:access_log, profile: alice) }
-  let!(:talk) { create(:talk1) }
-  let!(:registered_talk) { create(:registered_talk, profile: alice, talk: talk) }
-  let!(:form_item) { create(:form_item1)}
-  let!(:agreement) { create(:agreement, profile: alice, form_item_id: form_item.id) }
-  let!(:chat_message) { create(:message_from_alice, conference_id: cndt2020.id, profile: alice, room_id: talk.id)}
   let(:task) { 'util:cleanup_profiles' }
 
-  it 'delete profiles related of conference' do
-    @rake[task].invoke
-    expect(Profile.where(conference_id: cndt2020.id).size).to(eq(0))
+  describe "archived conference" do
+    let!(:cndt2020) { create(:cndt2020, :archived) }
+    let!(:alice) { create(:alice, :on_cndt2020, conference: cndt2020) }
+    let!(:access_log) { create(:access_log, profile: alice) }
+    let!(:talk) { create(:talk1) }
+    let!(:registered_talk) { create(:registered_talk, profile: alice, talk: talk) }
+    let!(:form_item) { create(:form_item1)}
+    let!(:agreement) { create(:agreement, profile: alice, form_item_id: form_item.id) }
+    let!(:chat_message) { create(:message_from_alice, conference_id: cndt2020.id, profile: alice, room_id: talk.id)}
+    let(:task) { 'util:cleanup_profiles' }
+
+    it 'delete profiles related of conference' do
+      @rake[task].invoke
+      expect(Profile.where(conference_id: cndt2020.id).size).to(eq(0))
+    end
+
+    it 'delete access logs related of profile' do
+      @rake[task].invoke
+      expect(AccessLog.where(profile_id: alice.id).size).to(eq(0))
+    end
+
+    it 'delete registered talks related of profile' do
+      @rake[task].invoke
+      expect(RegisteredTalk.where(profile_id: alice.id).size).to(eq(0))
+    end
+
+    it 'delete agreements related of profile' do
+      @rake[task].invoke
+      expect(Agreement.where(profile_id: alice.id).size).to(eq(0))
+    end
+
+    it 'doesn\'t delete chat messages related of profile' do
+      @rake[task].invoke
+      expect(ChatMessage.where(conference_id: cndt2020.id).size).to(eq(1))
+      expect(ChatMessage.find_by(conference_id: cndt2020.id).profile).to(eq(nil))
+    end
   end
 
-  it 'delete access logs related of profile' do
-    @rake[task].invoke
-    expect(AccessLog.where(profile_id: alice.id).size).to(eq(0))
+  describe "registered conference" do
+    let!(:cndt2020) { create(:cndt2020, :registered) }
+
+    it 'doesn\'t work' do
+      expect{@rake[task].invoke}.to(raise_error("cndt2020 is not archived yet"))
+    end
   end
 
-  it 'delete registered talks related of profile' do
-    @rake[task].invoke
-    expect(RegisteredTalk.where(profile_id: alice.id).size).to(eq(0))
+  describe "opened conference" do
+    let!(:cndt2020) { create(:cndt2020, :opened) }
+
+    it 'doesn\'t work' do
+      expect{@rake[task].invoke}.to(raise_error("cndt2020 is not archived yet"))
+    end
   end
 
-  it 'delete agreements related of profile' do
-    @rake[task].invoke
-    expect(Agreement.where(profile_id: alice.id).size).to(eq(0))
-  end
+  describe "closed conference" do
+    let!(:cndt2020) { create(:cndt2020, :closed) }
 
-  it 'doesn\'t delete chat messages related of profile' do
-    @rake[task].invoke
-    expect(ChatMessage.where(conference_id: cndt2020.id).size).to(eq(1))
-    expect(ChatMessage.find_by(conference_id: cndt2020.id).profile).to(eq(nil))
+    it 'doesn\'t work' do
+      expect{@rake[task].invoke}.to(raise_error("cndt2020 is not archived yet"))
+    end
   end
 end

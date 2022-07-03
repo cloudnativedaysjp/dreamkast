9b13cf6c104d5cd1d5ecb7fe3c45acfa119dd607
Kazuto Kusama <jacopen@gmail.com>
2022-07-03 20:33:55 +0900

    Make configurable issuer url

--- app/middlewares/json_web_token.rb
+++ app/middlewares/json_web_token.rb
@@ -6,7 +6,7 @@ class JsonWebToken
     JWT.decode(token, nil,
                true, # Verify the signature of this token
                algorithms: 'RS256',
-               iss: 'https://jacopen.auth0.com/', # ここに自身のドメインを指定する
+               iss: "https://#{AUTH0_DOMAIN}/", # ここに自身のドメインを指定する
                verify_iss: true,
                aud: Rails.application.secrets.auth0_api_audience,
                verify_aud: true) do |header|
@@ -15,7 +15,7 @@ class JsonWebToken
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get(URI('https://jacopen.auth0.com/.well-known/jwks.json')) # ここに自身のドメインを指定する
+    jwks_raw = Net::HTTP.get(URI("https://#{AUTH0_DOMAIN}/.well-known/jwks.json")) # ここに自身のドメインを指定する
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
       jwks_keys

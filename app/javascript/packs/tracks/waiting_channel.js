28ff88fea4833bef1ac9c0db37b1dcfa02cd33da
Kazuto Kusama <jacopen@gmail.com>
2022-07-23 00:17:15 +0900

    Show countdown before move

--- app/javascript/packs/tracks/waiting_channel.js
+++ app/javascript/packs/tracks/waiting_channel.js
@@ -1,20 +1,35 @@
-import  "./track_control.js"
+import "./track_control.js"
 import consumer from "./consumer"
 
 consumer.subscriptions.create("WaitingChannel", {
-  connected() {
-    console.log("waiting connected");
-  },
+    connected() {
+        console.log("waiting connected");
+    },
 
-  disconnected() {
-  },
+    disconnected() {},
 
-  received(data) {
-    console.log("received");
-    window.location.href = data["redirectTo"];
-  },
+    received(data) {
+        console.log("received");
+        var countdownNumberEl = document.getElementById('countdown-number');
+        var min = 3;
+        var max = 10
+        var countdown = Math.floor(Math.random() * (max + 1 - min)) + min;
 
-  update: function(message) {
-    return this.perform('update');
-  }
+        countdownNumberEl.textContent = countdown;
+
+        document.getElementById('countdown').classList.remove('hidden');
+        document.getElementById('countdown-circle').style.animation = `countdown ${countdown}s linear infinite forwards`;
+
+        setInterval(function() {
+            countdown = --countdown;
+            countdownNumberEl.textContent = countdown;
+            if (countdown == 0) {
+                window.location.href = data["redirectTo"];
+            }
+        }, 1000);
+    },
+
+    update: function(message) {
+        return this.perform('update');
+    }
 });

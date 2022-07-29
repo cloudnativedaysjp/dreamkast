3fc7de6e8499129d311a9e42d7c57cff6cffaeec
Kazuto Kusama <jacopen@gmail.com>
2022-07-29 22:41:47 +0900

    Saveしわすれ

--- db/fixtures/development/00_seeds.rb
+++ db/fixtures/development/00_seeds.rb
@@ -138,20 +138,20 @@ CloudNative Security Conference 2022 (CNSec2022) は、クラウドネイティ�
 そのような持続可能なセキュリティを実現するために、このイベントで様々な知見や技術を持ち寄って、安全で豊かな"Green"をともに目指してみませんか？
 EOS
   },
-  {
-    id: 7,
-    name: "CloudNative Days Tokyo 2022",
-    abbr: "cndt2022",
-    theme: "TBD",
-    copyright: '© CloudNative Days (Secretariat by Impress Corporation)',
-    privacy_policy: File.read(File.join(Rails.root, 'db/fixtures/production/privacy_policy_cndt2022.md')),
-    privacy_policy_for_speaker: File.read(File.join(Rails.root, 'db/fixtures/production/privacy_policy_for_speaker_cndt2022.md')),
-    coc: File.read(File.join(Rails.root, 'db/fixtures/production/coc.md')),
-    committee_name: "CloudNative Days Tokyo 2022 Committee",
-    about: <<'EOS'
-TBD
-EOS
-  },
+#   {
+#     id: 7,
+#     name: "CloudNative Days Tokyo 2022",
+#     abbr: "cndt2022",
+#     theme: "TBD",
+#     copyright: '© CloudNative Days (Secretariat by Impress Corporation)',
+#     privacy_policy: File.read(File.join(Rails.root, 'db/fixtures/production/privacy_policy_cndt2022.md')),
+#     privacy_policy_for_speaker: File.read(File.join(Rails.root, 'db/fixtures/production/privacy_policy_for_speaker_cndt2022.md')),
+#     coc: File.read(File.join(Rails.root, 'db/fixtures/production/coc.md')),
+#     committee_name: "CloudNative Days Tokyo 2022 Committee",
+#     about: <<'EOS'
+# TBD
+# EOS
+#   },
   {
     id: 10,
     name: "Test Event Winter 2020",

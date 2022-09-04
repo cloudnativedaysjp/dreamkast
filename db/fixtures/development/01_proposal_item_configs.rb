aaa925d1c19c2232535d6d2733c4f34ab1487152
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2022-09-04 20:27:59 +0900

    add proposal item configs for cndt2022

--- db/fixtures/development/01_proposal_item_configs.rb
+++ db/fixtures/development/01_proposal_item_configs.rb
@@ -549,3 +549,131 @@ ProposalItemConfig.seed(
     params: 'NG - いずれも公開不可（来場者限定のコンテンツ）'
   }
 )
+
+# =========== CNDT2022 =============
+ProposalItemConfig.seed(
+  [
+    [
+      {id: 59, params: 'architect - システム設計'},
+      {id: 60, params: 'developer - システム開発'},
+      {id: 61, params: 'app-developer - アプリケーション開発'},
+      {id: 62, params: 'operator/sys-admin - 運用管理/システム管理'},
+      {id: 63, params: 'CxO/biz - ビジネス層'},
+      {id: 64, params: 'その他'},
+    ].map do |item|
+      {
+        id: item[:id],
+        conference_id: 7,
+        type: 'ProposalItemConfigCheckBox',
+        label: 'assumed_visitor',
+        item_number: 1,
+        item_name: '想定受講者（★★）',
+        params: item[:params]
+      }
+    end,
+    [
+      {id: 65, params: 'Dev/QA（開発環境）'},
+      {id: 66, params: 'PoC（検証）'},
+      {id: 67, params: 'Production（本番環境）'},
+      {id: 68, params: 'Other'},
+    ].map do |item|
+      {
+        id: item[:id],
+        conference_id: 7,
+        type: 'ProposalItemConfigCheckBox',
+        label: 'execution_phase',
+        item_number: 2,
+        item_name: '実行フェーズ（★★）',
+        params: item[:params]
+      }
+    end,
+    [
+      {
+        id: 69,
+        params: 'All okay - スライド・動画両方ともに公開可',
+        description: 'イベント終了後に講演資料（スライドはslideshareなどにご自分でアップしてください）とアーカイブ動画を公開します。公開可否は来場者がセッションを選択する際の大きな判断材料となりますので事前に意思を確認させてください。動画はスライドと同期させた映像（例：https://www.youtube.com/watch?v=V21a3WMPC7s）を予定しています - After the event ends, we will publish the lecture materials (please upload yourself to slideshare etc) and archive videos. Please tell us in advance as visitors will be a big material to choose sessions.',
+        key: VideoAndSlidePublished::ALL_OK,
+        value: 'All okay - スライド・動画両方ともに公開可'
+      },
+      {
+        id: 70,
+        params: 'Only Slide - スライドのみ公開可',
+        description: '',
+        key: VideoAndSlidePublished::ONLY_SLIDE,
+        value: 'Only Slide - スライドのみ公開可'
+      },
+      {
+        id: 71,
+        params: 'NG - いずれも公開不可（来場者限定のコンテンツ）',
+        description: '',
+        key: VideoAndSlidePublished::ALL_NG,
+        value: 'NG - いずれも公開不可（来場者限定のコンテンツ）'
+      },
+      {
+        id: 72,
+        params: 'その他',
+        description: '',
+        key: VideoAndSlidePublished::OTHERS,
+        value: 'その他'
+      }
+    ].map do |item|
+      {
+        id: item[:id],
+        conference_id: 7,
+        type: 'ProposalItemConfigRadioButton',
+        label: 'whether_it_can_be_published',
+        item_number: 3,
+        item_name: 'スライドと動画の公開可否（★★）',
+        params: item[:params],
+        description: item[:description],
+        key: item[:key],
+        value: item[:value]
+      }
+    end,
+    [
+      {id: 73, params: '40min (full session)', description: 'ワークショップやLTなどを希望される場合はその他に希望時間を記入してください - If you are requesting a workshop or LT etc, please fill in the desired time in addition'},
+      {id: 74, params: '20min (half session)', description: ''}
+    ].map do |item|
+      {
+        id: item[:id],
+        conference_id: 7,
+        type: 'ProposalItemConfigRadioButton',
+        label: 'session_time',
+        item_number: 4,
+        item_name: '必要とする講演時間 - Session time you need（★）',
+        params: item[:params],
+        description: item[:description],
+      }
+    end,
+    [
+      {id: 75, params: 'JA', description: '英語での講演は、翻訳者や通訳機器の都合で会場やセッション時間に影響が出てくる可能性もあります - The session in English, may also affect the venue and session time'},
+      {id: 76, params: 'EN'},
+    ].map do |item|
+      {
+        id: item[:id],
+        conference_id: 7,
+        type: 'ProposalItemConfigRadioButton',
+        label: 'language',
+        item_number: 5,
+        item_name: '講演言語 - Language（★）',
+        params: item[:params],
+        description: item[:description],
+      }
+    end,
+    [
+      {id: 77, params: '現地登壇'},
+      {id: 78, params: 'オンライン登壇'},
+      {id: 79, params: '事前収録'},
+    ].map do |item|
+      {
+        id: item[:id],
+        conference_id: 7,
+        type: 'ProposalItemConfigRadioButton',
+        label: 'presentation_method',
+        item_number: 6,
+        item_name: '登壇方法の希望',
+        params: item[:params],
+      }
+    end
+  ].flatten
+)

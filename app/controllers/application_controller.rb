164d27976f7607075b75072cda615caf8749ad9c
YHojo <hojo.yosuke@gmail.com>
2022-09-11 22:30:42 +0900

    fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -117,11 +117,11 @@ class ApplicationController < ActionController::Base
   end
 
   def set_profile
-    if @current_user && (set_conference.opened? || set_conference.registered?)
-      @profile = Profile.find_by(email: @current_user[:info][:email], conference_id: set_conference.id)
-    else
-      @profile = Profile.set_guest
-    end
+    @profile = if @current_user && (set_conference.opened? || set_conference.registered?)
+                 Profile.find_by(email: @current_user[:info][:email], conference_id: set_conference.id)
+               else
+                 Profile.set_guest
+               end
   end
 
   def set_speaker

a667211c43cc2a4727cffc90b009983493ea6d8a
YHojo <hojo.yosuke@gmail.com>
2022-10-03 22:16:10 +0900

    add missing_method hooks

--- app/models/guest_profile.rb
+++ app/models/guest_profile.rb
@@ -17,4 +17,26 @@ class GuestProfile
   def save
     true
   end
+
+  # do not return method_missing
+  # for methods to which Profile instance can respond,
+  # but return an appropriate value
+  def method_missing(method, *args)
+    if Profile.new.respond_to?(method)
+      match = method.to_s.match(/(.*?)([?=!]?)$/)
+      case match[2]
+      when '='
+        return args
+      when '?'
+        return true
+      else
+        return nil
+      end
+    end
+    super
+  end
+
+  def respond_to_missing?(method, include_privete = false)
+    Profile.new.respond_to?(method) || super
+  end
 end

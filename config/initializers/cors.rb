a1517ef24c36bac512a7e19ad350c6fbd1f34944
Kazuto Kusama <jacopen@gmail.com>
2022-11-05 23:49:18 +0900

    Fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins(%r{https://.*\.cloudnativedays\.jp})
+    origins ENV['DREAMKAST_UI_BASE_URL'] || %r{https://.*\.cloudnativedays\.jp}
     resource '*', methods: :any, headers: :any
   end
 end

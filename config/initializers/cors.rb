4550e58c80fc0132418da54d46b29cecf94d9dd4
Kazuto Kusama <jacopen@gmail.com>
2022-11-05 23:46:32 +0900

    Fix CORS setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins ENV['DREAMKAST_UI_BASE_URL'] || 'https://*.cloudnativedays.jp'
+    origins(%r{https://.*\.cloudnativedays\.jp})
     resource '*', methods: :any, headers: :any
   end
 end

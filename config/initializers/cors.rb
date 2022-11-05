50a43c77aab14e0996f376d25314542d2e149570
Kazuto Kusama <jacopen@gmail.com>
2022-11-05 19:24:17 +0900

    Add rack-cors

---
+++ config/initializers/cors.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.insert_before(0, Rack::Cors) do
+  allow do
+    origins ENV['DREAMKAST_API_BASE_URL'] || 'https://*.cloudnativedays.jp'
+    resource '*', methods: :any, headers: :any
+  end
+end

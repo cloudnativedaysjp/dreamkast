3e0f98d6872736492374c86dbd3dae870ffada14
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2022-12-26 20:17:06 +0900

    update schema information

--- spec/factories/conferences.rb
+++ spec/factories/conferences.rb
@@ -18,7 +18,7 @@
 #  show_sponsors              :boolean          default(FALSE)
 #  show_timetable             :integer
 #  speaker_entry              :integer
-#  status                     :integer          default("registered"), not null
+#  status                     :integer          default(0), not null
 #  theme                      :text(65535)
 #  created_at                 :datetime         not null
 #  updated_at                 :datetime         not null

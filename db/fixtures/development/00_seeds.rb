ec72141d5df5741370e73b51ba81a7aac6fae4b1
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2023-03-12 13:23:51 +0900

    add rehersal 2

--- db/fixtures/development/00_seeds.rb
+++ db/fixtures/development/00_seeds.rb
@@ -231,6 +231,7 @@ ConferenceDay.seed(
   {id: 21, date: "2023-03-20", start_time: "12:20", end_time: "18:00", conference_id: 8, internal: false},
   {id: 22, date: "2023-02-07", start_time: "19:00", end_time: "21:00", conference_id: 8, internal: true}, #Pre event
   {id: 23, date: "2023-03-04", start_time: "09:00", end_time: "21:00", conference_id: 8, internal: true}, #Rehearsal
+  {id: 24, date: "2023-03-19", start_time: "09:00", end_time: "21:00", conference_id: 8, internal: true}, #Rehearsal2
 )
 
 FormItem.seed(

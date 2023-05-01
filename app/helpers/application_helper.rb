cf5b9c5b081838ac3005fbec2240d90b0db6e8bf
YHojo <hojo.yosuke@gmail.com>
2023-05-01 22:19:13 +0900

    cfp投票先をdk-weaverに切り替え

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -103,7 +103,9 @@ module ApplicationHelper
     end
   end
 
-  def vote_api_url(id)
-    File.join(ENV['DREAMKAST_API_ADDR'], 'api/v1/talks', id, 'vote')
+  def vote_api_url
+    [
+      ENV.fetch('DREAMKAST_WEAVER_ADDR', 'http://localhost:8080'), 'query'
+    ].join('/')
   end
 end
--- app/javascript/packs/vote_cfp.js
+++ app/javascript/packs/vote_cfp.js
@@ -24,13 +24,27 @@ document.getElementById('vote').addEventListener('click', function() {
     const voteUrl = document.getElementById('vote').getAttribute('vote_url');
 
     const eventAbbr = document.getElementById('vote').getAttribute('event_name');
+    const talkId = parseInt(document.getElementById('vote').getAttribute('talk_id'));
     const method = 'POST'
-    const body = JSON.stringify({'eventAbbr': eventAbbr});
-    const headers = {'Content-Type': 'application/json'};
+    const query = `mutation {
+          vote(input: {
+            confName: ${eventAbbr}
+            talkId: ${talkId}
+        })
+    }`;
+    const body = JSON.stringify({ query });
+    const headers = { 'Content-Type': 'application/json' };
 
-    fetch(voteUrl,{ method, headers, body});
-    setVotedId(parseInt(document.getElementById('vote').getAttribute('talk_id')));
-    toggleVoted();
+    fetch(voteUrl,{ method, headers, body})
+        .then(r => r.json())
+        .then(data => {
+          if (data.hasOwnProperty('errors')) {
+              alert("投票を受け付けられませんでした。\nしばらく時間をおいてから再度の投票をお願いします。");
+          } else {
+              setVotedId(talkId);
+              toggleVoted();
+          }
+        })
     return false;
 });
 

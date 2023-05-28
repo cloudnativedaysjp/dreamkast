95845d9762ecd1ca61cd540e0b6e823146ec0967
inductor <kela@inductor.me>
2023-05-28 14:27:04 +0900

    use length

--- db/migrate/20230528051104_add_multiple_indexes.rb
+++ db/migrate/20230528051104_add_multiple_indexes.rb
@@ -3,7 +3,7 @@ class AddMultipleIndexes < ActiveRecord::Migration[7.0]
     add_index :conferences, :abbr
     add_index :conferences, [:abbr, :conference_status]
     add_index :proposal_item_configs, [:conference_id, :item_number]
-    add_index :speakers, [:conference_id, :email]
+    add_index :speakers, [:conference_id, :email], length: { email: 255 }
     add_index :talk_categories, :conference_id
     add_index :talk_difficulties, :conference_id
     add_index :talks_speakers, :speaker_id

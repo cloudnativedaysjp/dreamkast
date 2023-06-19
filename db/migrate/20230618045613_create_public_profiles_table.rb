f728a5e860a70163ca33694547050445562503ce
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2023-06-19 20:39:59 +0900

    fix: type of avatar_data column

--- db/migrate/20230618045613_create_public_profiles_table.rb
+++ db/migrate/20230618045613_create_public_profiles_table.rb
@@ -5,7 +5,7 @@ class CreatePublicProfilesTable < ActiveRecord::Migration[7.0]
       t.string  :nickname,    type: :string,  default: ""
       t.string  :twitter_id,  type: :string,  default: ""
       t.string  :github_id,   type: :string,  default: ""
-      t.string  :avatar_data, type: :text
+      t.text  :avatar_data, type: :text
       t.boolean :is_public,   type: :boolean, default: false
     end
   end

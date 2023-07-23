a78abf9e7dd786a49484b47248fa13d2cd799c2c
Hiroki Okui <hiroki.okui@gmail.com>
2023-07-23 19:05:16 +0900

    Update sentry.rb
    sentryのsampling rateを1%に下げました

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,8 +6,8 @@ Sentry.init do |config|
 
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
-  config.traces_sample_rate = 0.2
-  config.profiles_sample_rate = 0.2
+  config.traces_sample_rate = 0.01
+  config.profiles_sample_rate = 0.01
   # set the instrumenter to use OpenTelemetry instead of Sentry
   config.instrumenter = :otel
 end

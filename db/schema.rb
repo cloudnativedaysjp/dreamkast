0c2348cd57df198bf40c8c3cafc0ce1103324f37
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2023-09-17 21:03:23 +0900

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_15_101121) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_16_121212) do
   create_table "access_logs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "sub"

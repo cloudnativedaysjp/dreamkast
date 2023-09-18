8531eaaddb02a47d2b27bdcc15785161976acf1e
Kazuto Kusama <jacopen@gmail.com>
2023-09-19 02:43:33 +0900

    Remove CICD2021 specific commits

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -263,9 +263,6 @@ class Talk < ApplicationRecord
   end
 
   def time
-    # CICD2021は全セッション40分固定で、talk_timeを持たせていないため
-    return 40 if conference.abbr == 'cicd2021'
-
     # CNDT2021移行はセッションの時間をProposalItemで管理するので、ProposalItemにsession_timeがあればそこからセッション時間を取得して返す
     session_time = proposal_items.find_by(label: 'session_time')
     return ProposalItemConfig.find(session_time.params.to_i).params.split('min')[0].to_i if session_time

681a9dcae5bf0bd8c6b112dbe108d88e63c86638
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2023-10-08 16:53:52 +0900

    feat: display copy icon after 5 sec

--- app/javascript/packs/copy.js
+++ app/javascript/packs/copy.js
@@ -1,10 +1,18 @@
 window.onload = () => {
-
     document.getElementsByClassName('copy_button').forEach((elm) => {
         elm.addEventListener('click', (e) => {
-            navigator.clipboard.writeText(e.target.parentElement.parentElement.querySelector('.dest_url').textContent)
+            const url = e.target.parentElement.parentElement.querySelector('.dest_url').textContent
+            const parent = e.target.parentElement
+            const icon = e.target
+
+            navigator.clipboard.writeText(url)
                 .then(() => {
+                    console.log("Copied to clipboard successfully!")
                     e.target.parentElement.textContent = "Copied"
+                    setTimeout(() => {
+                        parent.textContent = ""
+                        parent.appendChild(icon)
+                    }, 5000)
                 })
                 .catch((error) => {
                     console.error("Failed to copy", error);

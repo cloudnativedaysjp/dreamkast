cd0bc4cafc7cddc3d8e24b36713690e66b87ab85
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2023-10-30 15:31:49 +0900

    fix: session_timeの値はparamsではなくvalueを見るべき

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -274,7 +274,7 @@ class Talk < ApplicationRecord
 
     # CNDT2021移行はセッションの時間をProposalItemで管理するので、ProposalItemにsession_timeがあればそこからセッション時間を取得して返す
     session_time = proposal_items.find_by(label: 'session_time')
-    return ProposalItemConfig.find(session_time.params.to_i).params.split('min')[0].to_i if session_time
+    return ProposalItemConfig.find(session_time.params.to_i).value.to_i if session_time
 
     # プロポーザルでセッションの時間を選択するカンファレンスと選択しないカンファレンスがあり、選択しない場合はデフォルト値として40分とする
     talk_time.present? ? talk_time.time_minutes : 40

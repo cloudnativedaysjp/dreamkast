f2e99b073eeb9840cbed702e77ee289a2127de70
Kazuto Kusama <jacopen@gmail.com>
2023-11-16 20:51:22 +0900

    Add CORS for web-intermission

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins ENV['DREAMKAST_UI_BASE_URL'] || %r{https://.*\.cloudnativedays\.jp}
+    origins ENV['DREAMKAST_UI_BASE_URL'] || %r{https://.*\.cloudnativedays\.jp}, %r{https://emtec-intermission-git-.*-emtec\.vercel\.app}, %r{https://emtec-intermission\.vercel\.app}
     resource '*', methods: :any, headers: :any
   end
 end

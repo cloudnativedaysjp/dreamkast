5d26f0f63714c921a5b33416fbbf236c9c7421c7
Kazuto Kusama <jacopen@gmail.com>
2023-11-30 17:35:41 +0900

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins ENV['DREAMKAST_UI_BASE_URL'] || %r{https://.*\.cloudnativedays\.jp}, %r{https://emtec-intermission-git-.*-emtec\.vercel\.app}, %r{https://emtec-intermission\.vercel\.app}
+    origins ENV['DREAMKAST_UI_BASE_URL'] || %r{https://.*\.cloudnativedays\.jp}, %r{https://emtec-intermission-git-.*-emtec\.vercel\.app}, %r{https://emtec-intermission\.vercel\.app}, %r{http://localhost:3000}
     resource '*', methods: :any, headers: :any
   end
 end

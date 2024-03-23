640f222d01b1585b0f558a7fae2214cf94b9b070
Takafumi ONAKA <takafumi.onaka@gmail.com>
2024-03-23 16:40:26 +0900

    Remove unintentional `integer` column from `rooms` table
    See: PR #1559.

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -4,7 +4,6 @@
 #
 #  id              :bigint           not null, primary key
 #  description     :text(65535)
-#  integer         :integer          default(0), not null
 #  name            :string(255)      not null
 #  number_of_seats :integer          default(0), not null
 #  created_at      :datetime         not null
@@ -21,10 +20,9 @@
 #
 #  fk_rails_...  (conference_id => conferences.id)
 #  fk_rails_...  (track_id => tracks.id)
-
+#
 FactoryBot.define do
   factory :room, class: Room do
-    integer { 1 }
     name { 'ONLINE' }
     number_of_seats { 100 }
     conference_id { 1 }

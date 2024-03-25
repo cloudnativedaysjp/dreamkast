78df1bc419195cb0fb448c927f2e3f9278cf7303
Takafumi ONAKA <takafumi.onaka@gmail.com>
2024-03-25 11:17:33 +0900

    annotate

--- spec/models/speaker_announcement_spec.rb
+++ spec/models/speaker_announcement_spec.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: speaker_announcements
+#
+#  id            :bigint           not null, primary key
+#  body          :text(65535)      not null
+#  publish       :boolean          default(FALSE)
+#  publish_time  :datetime         not null
+#  receiver      :integer          default("person"), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  conference_id :bigint           not null
+#
+# Indexes
+#
+#  index_speaker_announcements_on_conference_id  (conference_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (conference_id => conferences.id)
+#
 require 'rails_helper'
 
 RSpec.describe(SpeakerAnnouncement, type: :model) do

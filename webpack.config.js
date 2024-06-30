5c461c07899729567290f414a5bc062c902bb5cb
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2024-06-30 19:03:58 +0900

    include image

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         test: /\.(?:sa|sc|c)ss$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|eot|woff2|woff|ttf|svg)$/i,
+        use: 'file-loader',
+      },
     ],
   },
   resolve: {

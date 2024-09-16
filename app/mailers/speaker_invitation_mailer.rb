7c0dc02e56e46c5351be61b0bda0398aedf6900c
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2024-09-16 09:00:42 +0900

    fix: use https if rails env is production

--- app/mailers/speaker_invitation_mailer.rb
+++ app/mailers/speaker_invitation_mailer.rb
@@ -9,7 +9,13 @@ class SpeakerInvitationMailer < ApplicationMailer
     @invited_by = invited_by
     @talk = talk
     @invitation = invitation
-    @new_accept_url = speaker_invitation_accepts_invite_url(event: @conference.abbr, token: @invitation.token)
+    @new_accept_url = speaker_invitation_accepts_invite_url(event: @conference.abbr, token: @invitation.token, protocol:)
     mail(to: @invitation.email, subject: "#{@conference.name} 共同スピーカー招待")
+  end
+
+  private
+
+  def protocol
+    Rails.env.production? ? 'https' : 'http'
   end
 end

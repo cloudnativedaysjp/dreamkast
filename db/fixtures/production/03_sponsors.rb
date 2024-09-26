fee7661b40893160a632ad2ecfa72956997d80a5
Kazuto Kusama <jacopen@gmail.com>
2024-09-26 15:31:43 +0900

    Add sponsor type into seed

---
+++ db/fixtures/production/03_sponsors.rb
@@ -0,0 +1,44 @@
+SponsorType.seed(
+  {
+    id: 71,
+    conference_id: 11,
+    name: "Diamond",
+    order: 1,
+  },
+  {
+    id: 72,
+    conference_id: 11,
+    name: "Platinum",
+    order: 2,
+  },
+  {
+    id: 73,
+    conference_id: 11,
+    name: "Gold",
+    order: 3,
+  },
+  {
+    id: 74,
+    conference_id: 11,
+    name: "Booth",
+    order: 4,
+  },
+  {
+    id: 75,
+    conference_id: 11,
+    name: "Tool",
+    order: 6,
+  },
+  {
+    id: 76,
+    conference_id: 11,
+    name: "Support",
+    order: 7,
+  },
+  {
+    id: 77,
+    conference_id: 11,
+    name: "CM",
+    order: 5,
+  },
+)

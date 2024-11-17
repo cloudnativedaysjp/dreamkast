6a465f5aeac69e6a6175d0252f5c421c4ef042bd
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2024-11-17 15:06:43 +0900

    chore: set speaker information

--- app/controllers/api/v1/check_in_conferences_controller.rb
+++ app/controllers/api/v1/check_in_conferences_controller.rb
@@ -9,10 +9,11 @@ class Api::V1::CheckInConferencesController < ApplicationController
   def create
     @params = check_in_conferences_params(JSON.parse(request.body.read, { symbolize_names: true }))
     attendee = Profile.find(@params[:profileId])
+    speaker = Speaker.find_by(email: attendee.email, conference_id: conference.id)
     check_in_timestamp = Time.zone.at(@params[:checkInTimestamp])
     @check_in = CheckInConference.new(profile: attendee, conference:, check_in_timestamp:, scanner_profile_id: @profile.id)
     conference = Conference.find_by(abbr: @params[:eventAbbr])
-    GenerateEntrysheetJob.perform_later(conference.id, attendee.id, nil)
+    GenerateEntrysheetJob.perform_later(conference.id, attendee.id, speaker.id)
 
     if @check_in.save
       render(json: @check_in, status: :created)

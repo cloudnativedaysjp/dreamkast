6c037d8a75698238901620cebca8a7493acbc169
さっちゃん <ne-sachirou@hatena.ne.jp>
2025-07-10 20:22:00 +0900

    gem

--- config/initializers/otel.rb
+++ config/initializers/otel.rb
@@ -1,6 +1,32 @@
-OpenTelemetry::SDK.configure do |c|
-  c.use_all
-  c.add_span_processor(Sentry::OpenTelemetry::SpanProcessor.instance)
-end
+# OpenTelemetry configuration for Mackerel
+require 'opentelemetry/sdk'
+require 'opentelemetry/exporter/otlp'
+require 'opentelemetry/instrumentation/all'
 
-OpenTelemetry.propagation = Sentry::OpenTelemetry::Propagator.new
+OpenTelemetry::SDK.configure do |c|
+  c.service_name = 'dreamkast'
+  c.service_version = '1.0.0'
+
+  c.resource = OpenTelemetry::SDK::Resources::Resource.create(
+    OpenTelemetry::SemanticConventions::Resource::DEPLOYMENT_ENVIRONMENT => Rails.env.to_s,
+    OpenTelemetry::SemanticConventions::Resource::HOST_NAME => Socket.gethostname
+  )
+
+  if ENV['MACKEREL_APIKEY'].present?
+    exporter = OpenTelemetry::Exporter::OTLP::Exporter.new(
+      endpoint: 'https://otlp-vaxila.mackerelio.com/v1/traces',
+      headers: {
+        'Accept' => '*/*',
+        'Mackerel-Api-Key' => ENV['MACKEREL_APIKEY']
+      }
+    )
+  else
+    Rails.logger.warn 'MACKEREL_APIKEY not set - using console exporter for OpenTelemetry'
+    exporter = OpenTelemetry::SDK::Trace::Export::ConsoleSpanExporter.new
+  end
+  c.add_span_processor(
+    OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(exporter)
+  )
+
+  c.use_all
+end

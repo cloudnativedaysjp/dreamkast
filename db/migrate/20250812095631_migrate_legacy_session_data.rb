d4f6c22a0c71d836487a068625fb59ba7180698a
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2025-08-20 22:59:36 +0900

    wip

--- db/migrate/20250812095631_migrate_legacy_session_data.rb
+++ db/migrate/20250812095631_migrate_legacy_session_data.rb
@@ -4,112 +4,24 @@ class MigrateLegacySessionData < ActiveRecord::Migration[8.0]
     return if Rails.env.test?
     
     say_with_time "Migrating legacy session data to new session attributes system" do
-      # Create temporary models for migration
-      temp_talk_attribute = Class.new(ActiveRecord::Base) do
-        self.table_name = 'talk_attributes'
-      end
-      
-      temp_talk_attribute_association = Class.new(ActiveRecord::Base) do
-        self.table_name = 'talk_attribute_associations'
-      end
-      
-      temp_talk = Class.new(ActiveRecord::Base) do
-        self.table_name = 'talks'
-      end
-      
       # Find attribute IDs
-      keynote_attr = temp_talk_attribute.find_by(name: 'keynote')
-      sponsor_attr = temp_talk_attribute.find_by(name: 'sponsor')
-      intermission_attr = temp_talk_attribute.find_by(name: 'intermission')
-      
-      migrated_count = 0
-      
-      # Migrate KeynoteSession talks (if using STI)
-      # Use raw SQL to avoid STI class loading issues
-      if temp_talk.column_names.include?('type')
-        keynote_talks = temp_talk.connection.execute(
-          "SELECT id FROM talks WHERE type = 'KeynoteSession'"
-        )
-        keynote_talks.each do |row|
-          talk_id = row.is_a?(Array) ? row[0] : row['id']
-          temp_talk_attribute_association.find_or_create_by!(
-            talk_id: talk_id,
-            session_attribute_id: keynote_attr.id
-          )
-          migrated_count += 1
+      keynote_attr = TalkAttribute.find_by(name: 'keynote')
+      sponsor_attr = TalkAttribute.find_by(name: 'sponsor')
+      intermission_attr = TalkAttribute.find_by(name: 'intermission')
+
+      Talk.all.each do |talk|
+        case talk.type
+        when "KeynoteSession"
+          TalkAttributeAssociation.create!(talk_id: talk.id, talk_attribute_id: keynote_attr.id)
+          if talk.sponsor_session?
+            TalkAttributeAssociation.create!(talk_id: talk.id, talk_attribute_id: sponsor_attr.id)
+          end
+        when "SponsorSession"
+          TalkAttributeAssociation.create!(talk_id: talk.id, talk_attribute_id: sponsor_attr.id)
+        when "Intermission"
+          TalkAttributeAssociation.create!(talk_id: talk.id, talk_attribute_id: intermission_attr.id)
         end
       end
-      
-      # Migrate SponsorSession talks (if using STI)
-      if temp_talk.column_names.include?('type')
-        sponsor_talks = temp_talk.connection.execute(
-          "SELECT id FROM talks WHERE type = 'SponsorSession'"
-        )
-        sponsor_talks.each do |row|
-          talk_id = row.is_a?(Array) ? row[0] : row['id']
-          temp_talk_attribute_association.find_or_create_by!(
-            talk_id: talk_id,
-            session_attribute_id: sponsor_attr.id
-          )
-          migrated_count += 1
-        end
-      end
-      
-      # Migrate talks with sponsor_id
-      # Use raw SQL to avoid STI issues
-      sponsor_id_talks = temp_talk.connection.execute(
-        "SELECT id FROM talks WHERE sponsor_id IS NOT NULL"
-      )
-      sponsor_id_talks.each do |row|
-        talk_id = row.is_a?(Array) ? row[0] : row['id']
-        # Skip if already migrated from STI
-        next if temp_talk_attribute_association.exists?(
-          talk_id: talk_id,
-          session_attribute_id: sponsor_attr.id
-        )
-        
-        temp_talk_attribute_association.find_or_create_by!(
-          talk_id: talk_id,
-          session_attribute_id: sponsor_attr.id
-        )
-        migrated_count += 1
-      end
-      
-      # Migrate Intermission talks (if using STI)
-      if temp_talk.column_names.include?('type')
-        intermission_talks = temp_talk.connection.execute(
-          "SELECT id FROM talks WHERE type = 'Intermission'"
-        )
-        intermission_talks.each do |row|
-          talk_id = row.is_a?(Array) ? row[0] : row['id']
-          temp_talk_attribute_association.find_or_create_by!(
-            talk_id: talk_id,
-            session_attribute_id: intermission_attr.id
-          )
-          migrated_count += 1
-        end
-      end
-      
-      # Migrate talks with abstract = 'intermission'
-      # Use raw SQL to avoid STI issues
-      abstract_intermission_talks = temp_talk.connection.execute(
-        "SELECT id FROM talks WHERE abstract = 'intermission'"
-      )
-      abstract_intermission_talks.each do |row|
-        talk_id = row.is_a?(Array) ? row[0] : row['id']
-        # Skip if already migrated from STI
-        next if temp_talk_attribute_association.exists?(
-          talk_id: talk_id,
-          session_attribute_id: intermission_attr.id
-        )
-        
-        temp_talk_attribute_association.find_or_create_by!(
-          talk_id: talk_id,
-          session_attribute_id: intermission_attr.id
-        )
-        migrated_count += 1
-      end
-      
       say "Migrated #{migrated_count} session attributes"
     end
   end
@@ -118,10 +30,6 @@ class MigrateLegacySessionData < ActiveRecord::Migration[8.0]
     # Skip if running in test environment
     return if Rails.env.test?
     
-    temp_talk_attribute_association = Class.new(ActiveRecord::Base) do
-      self.table_name = 'talk_talk_attributes'
-    end
-    
-    temp_talk_attribute_association.delete_all
+    TalkAttributeAssociation.delete_all
   end
 end

6569fed53256023a39547056e26bc05206b5346d
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2025-08-30 11:21:29 +0900

    add index

--- db/migrate/20250812095327_create_talk_types.rb
+++ db/migrate/20250812095327_create_talk_types.rb
@@ -4,7 +4,6 @@ class CreateTalkTypes < ActiveRecord::Migration[8.0]
     add_column :talk_types, :display_name, :string, null: false, limit: 100
     add_column :talk_types, :description, :text
     add_column :talk_types, :is_exclusive, :boolean, default: false, null: true
-    # add_index :talk_types, :name, unique: true
-    # add_index :talk_types, :is_exclusive
+    add_index :talk_types, :is_exclusive
   end
 end

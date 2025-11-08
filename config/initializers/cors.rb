c7bf7a957f39dc14bba5bdca167a51d50b161524
Kazuto Kusama <jacopen@gmail.com>
2025-11-08 22:18:14 +0900

    Update config/initializers/cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins '*'
+    origins 'https://im.emtec.tv/'
 
     resource '*',
              headers: :any,

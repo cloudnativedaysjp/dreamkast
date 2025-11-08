b7c3d8574b537235d2d0dfc07c958a715625ae8a
dero - Kazuma Tomatsu <52665786+dero1to@users.noreply.github.com>
2025-11-08 21:24:39 +0900

    Apply suggestion from @dero1to

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins(%r{/https:\/\/.*\.emtec\.tv$/}, %r{/https:\/\/.*\emtec-intermission\.pages\.dev$/})
+    origins(%r{https://.*\.emtec\.tv$}, %r{https://.*\.emtec-intermission\.pages\.dev$})
 
     resource '*',
              headers: :any,

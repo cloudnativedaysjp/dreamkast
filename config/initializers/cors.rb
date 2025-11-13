f71726b34dd6857b4f1098f2014ac3d937263806
dero - Kazuma Tomatsu <52665786+dero1to@users.noreply.github.com>
2025-11-13 13:08:57 +0900

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins 'https://im.emtec.tv/'
+    origins 'https://im.emtec.tv'
 
     resource '*',
              headers: :any,

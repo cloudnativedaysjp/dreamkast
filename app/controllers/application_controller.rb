778925aeb5af85cdf1f92f47d59a210477967b9c
さっちゃん <ne-sachirou@hatena.ne.jp>
2025-11-18 17:49:00 +0900

    GET /api/v1/:event/my_profileでundefined method `id' for nilエラーを防ぐ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -155,7 +155,7 @@ class ApplicationController < ActionController::Base
 
   def set_profile
     @profile = if current_user && (set_conference.opened? || set_conference.registered? || set_conference.closed?)
-                 Profile.find_by(email: current_user[:info][:email], conference_id: set_conference.id)
+                 Profile.find_by(email: current_user[:info][:email], conference_id: set_conference.id) || GuestProfile.new
                else
                  GuestProfile.new
                end

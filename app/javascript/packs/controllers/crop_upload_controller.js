ae7139a4b8155ed471e941e229ce1c4f81ae4324
Kazuto Kusama <jacopen@gmail.com>
2025-12-08 03:17:50 +0900

    WIP

--- app/javascript/packs/controllers/crop_upload_controller.js
+++ app/javascript/packs/controllers/crop_upload_controller.js
@@ -1,76 +1,76 @@
-import { Controller } from "@hotwired/stimulus"
-import Cropper from "cropperjs"
-import { Uppy } from '@uppy/core'
+import { Controller } from "@hotwired/stimulus";
+import Cropper from "cropperjs";
+import { Uppy } from "@uppy/core";
 // Import Uppy CSS
-import '@uppy/core/dist/style.css'
-import '@uppy/progress-bar/dist/style.css'
-import '@uppy/informer/dist/style.css'
-import '@uppy/file-input/dist/style.css'
-import FileInput from '@uppy/file-input'
-import Informer from '@uppy/informer'
-import ProgressBar from '@uppy/progress-bar'
-import ThumbnailGenerator from '@uppy/thumbnail-generator'
-import XHRUpload from '@uppy/xhr-upload'
+import "@uppy/core/css/style.min.css";
+import "@uppy/progress-bar/dist/style.css";
+import "@uppy/informer/dist/style.css";
+import "@uppy/file-input/dist/style.css";
+import FileInput from "@uppy/file-input";
+import Informer from "@uppy/informer";
+import ProgressBar from "@uppy/progress-bar";
+import ThumbnailGenerator from "@uppy/thumbnail-generator";
+import XHRUpload from "@uppy/xhr-upload";
 
 export default class extends Controller {
-    static targets = [ "fileInput" ]
+  static targets = ["fileInput"];
 
-    connect() {
-        this.fileInputTargets.forEach(fileInput => {
-            console.log(fileInput)
-            this.cropUpload(fileInput)
-        })
-    }
+  connect() {
+    this.fileInputTargets.forEach((fileInput) => {
+      console.log(fileInput);
+      this.cropUpload(fileInput);
+    });
+  }
 
-    cropUpload(fileInput) {
-        console.log(fileInput)
-        let formGroup = fileInput.parentNode
-        console.log(formGroup)
-        let hiddenInput = document.querySelector('.upload-data')
-        console.log(hiddenInput)
-        let imagePreview = document.querySelector('.image-preview img')
-        console.log(imagePreview)
+  cropUpload(fileInput) {
+    console.log(fileInput);
+    let formGroup = fileInput.parentNode;
+    console.log(formGroup);
+    let hiddenInput = document.querySelector(".upload-data");
+    console.log(hiddenInput);
+    let imagePreview = document.querySelector(".image-preview img");
+    console.log(imagePreview);
 
-        formGroup.removeChild(fileInput)
+    formGroup.removeChild(fileInput);
 
-        let uppy = new Uppy({
-            autoProceed: true,
-        })
-            .use(FileInput, {
-                target: formGroup,
-                locale: { strings: { chooseFiles: 'Choose file' } },
-            })
-            .use(Informer, {
-                target: formGroup,
-            })
-            .use(ProgressBar, {
-                target: imagePreview.parentNode,
-            })
-            .use(ThumbnailGenerator, {
-                thumbnailWidth: 600,
-            })
-            .use(XHRUpload, {
-                endpoint: '/upload/avatar',
-            })
+    let uppy = new Uppy({
+      autoProceed: true,
+    })
+      .use(FileInput, {
+        target: formGroup,
+        locale: { strings: { chooseFiles: "Choose file" } },
+      })
+      .use(Informer, {
+        target: formGroup,
+      })
+      .use(ProgressBar, {
+        target: imagePreview.parentNode,
+      })
+      .use(ThumbnailGenerator, {
+        thumbnailWidth: 600,
+      })
+      .use(XHRUpload, {
+        endpoint: "/upload/avatar",
+      });
 
-        uppy.on('upload-success', function (file, response) {
-            imagePreview.src = response.uploadURL
+    uppy.on("upload-success", function (file, response) {
+      imagePreview.src = response.uploadURL;
 
-            hiddenInput.value = JSON.stringify(response.body['data'])
+      hiddenInput.value = JSON.stringify(response.body["data"]);
 
-            let copper = new Cropper(imagePreview, {
-                aspectRatio: 1,
-                viewMode: 1,
-                guides: false,
-                autoCropArea: 1.0,
-                background: false,
-                checkCrossOrigin: false,
-                crop: function (event) {
-                    let data = JSON.parse(hiddenInput.value)
-                    data['metadata']['crop'] = event.detail
-                    hiddenInput.value = JSON.stringify(data)
-                }
-            })
-        })
-    }
+      let copper = new Cropper(imagePreview, {
+        aspectRatio: 1,
+        viewMode: 1,
+        guides: false,
+        autoCropArea: 1.0,
+        background: false,
+        checkCrossOrigin: false,
+        crop: function (event) {
+          let data = JSON.parse(hiddenInput.value);
+          data["metadata"]["crop"] = event.detail;
+          hiddenInput.value = JSON.stringify(data);
+        },
+      });
+    });
+  }
 }

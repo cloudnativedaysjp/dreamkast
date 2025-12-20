2600f52e280ac4a9b1d02be75298c2766cf3222c
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2025-12-20 17:10:23 +0900

    Update user model associations to use has_one relationships for profile, sponsor_contact, and admin_profile, enhancing data integrity and simplifying user-related queries.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :profiles, dependent: :destroy
+  has_one :profile, dependent: :destroy
   has_many :speakers, dependent: :destroy
-  has_many :sponsor_contacts, dependent: :destroy
-  has_many :admin_profiles, dependent: :destroy
+  has_one :sponsor_contact, dependent: :destroy
+  has_one :admin_profile, dependent: :destroy
 
   validates :sub, presence: true, uniqueness: true
   validates :email, presence: true

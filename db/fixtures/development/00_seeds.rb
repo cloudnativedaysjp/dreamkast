57ca086dd821bf4464b2366b8392cd134e4b5590
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2026-03-01 18:18:32 +0900

    Fix announcement seeds receiver

--- db/fixtures/development/00_seeds.rb
+++ db/fixtures/development/00_seeds.rb
@@ -926,16 +926,16 @@ Link.seed(
 )
 
 Announcement.seed(
-  {id: 1, conference_id: 1, publish_time: "2020-08-24 10:00:00", publish: true, body: <<'EOS'
+  {id: 1, conference_id: 1, publish_time: "2020-08-24 10:00:00", publish: true, receiver: :all_attendee, body: <<'EOS'
 9/2（水）19:00-20:30に、プレイベントとして、CNDT2020 Rejektsを開催します！CNDT2020にお申込の方はどなたでもご参加できます！ぜひご視聴ください！,
 EOS
   },
-  {id: 2, conference_id: 1, publish_time: "2020-08-20 10:00:00", publish: true, body: <<'EOS'
+  {id: 2, conference_id: 1, publish_time: "2020-08-20 10:00:00", publish: true, receiver: :all_attendee, body: <<'EOS'
 最終セッションの実施時間を18:00-18:40に変更致しました。それに伴い、イベントの終了時間は19:00となります(ask the speaker含む）
 <a href="https://event.cloudnativedays.jp/cndt2020/talks/66" target="_blank">「Cloud Foundry on K8sでクラウドネイティブ始めませんか？」（有元 久住 / SUSE )</a>のセッション時間が、9/9 16:00-16:40に変更になりました。予定が重複する場合は、登録セッションを変更してください
 EOS
 },
-  {id: 3, conference_id: 4, publish_time: "2021-10-11 10:00:00", publish: true, body: <<'EOS'
+  {id: 3, conference_id: 4, publish_time: "2021-10-11 10:00:00", publish: true, receiver: :all_attendee, body: <<'EOS'
 <a href="/cndt2021/" target="_blank">CloudNative Days Tokyo 2021</a>開催に向けて、10/18（月）19:00からプレイベントを実施します!
 EOS
 }

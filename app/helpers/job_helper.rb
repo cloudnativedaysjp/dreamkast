7eb42df66e03f6d5c250c8d55e545be051875ab3
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2026-03-05 21:38:15 +0900

    Harden SQS queue URL resolution for missing env vars

--- app/helpers/job_helper.rb
+++ app/helpers/job_helper.rb
@@ -1,14 +1,20 @@
 module JobHelper
   include EnvHelper
 
+  DEFAULT_FIFO_QUEUE_URL = 'http://localhost:4566/000000000000/fifo.fifo'.freeze
+
   def sqs_queue_url
-    if ENV['SQS_FIFO_QUEUE_URL'].blank?
-      cli = Aws::SQS::Client.new(region: 'ap-northeast-1')
-      result = cli.get_queue_url({ queue_name: "review_app_#{review_app_number}" + '.fifo' })
-      raise(result.error) unless result.successful?
-      result.queue_url
-    else
-      ENV['SQS_FIFO_QUEUE_URL']
-    end
+    return ENV['SQS_FIFO_QUEUE_URL'] if ENV['SQS_FIFO_QUEUE_URL'].present?
+
+    namespace = ENV['DREAMKAST_NAMESPACE'].to_s
+    return DEFAULT_FIFO_QUEUE_URL if namespace.blank?
+
+    cli = Aws::SQS::Client.new(region: 'ap-northeast-1')
+    result = cli.get_queue_url({ queue_name: "review_app_#{review_app_number}.fifo" })
+    raise(result.error) unless result.successful?
+    result.queue_url
+  rescue StandardError => e
+    warn("Failed to resolve SQS queue URL (#{e.class}: #{e.message}), fallback to #{DEFAULT_FIFO_QUEUE_URL}")
+    DEFAULT_FIFO_QUEUE_URL
   end
 end
